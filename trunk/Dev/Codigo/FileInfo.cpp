8a3f74b65052da7de9bb17c7003be0f64a3f75cf
pablonazareno <pablonazareno@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-03-25 03:40:55 +0000

    -Correcciones y reimplemetacion de FileInfo y FileManager -Creacion de Exception IOException -Scratch del Demonio

--- trunk/Dev/Codigo/FileInfo.cpp
+++ trunk/Dev/Codigo/FileInfo.cpp
@@ -1,52 +1,60 @@
 #include "FileInfo.h"
-/*
-	// GETERS y SETERS NECESARIOS
-	//char* getFileName(void){return _name;};
-	//void setFile(FILE* file){_file = file;};
-	//FILE* getFile(void){return _file;};
+
+namespace FileManager{
+
+	FileInfo::FileInfo(char* fileName){
+		_file= NULL;
+		_name= cloneStr(fileName);
+	}
 	
-	void write(void* source, int size);
-*/
-FileInfo::FileInfo(char* fileName){
-	_file= NULL;
-	_name= cloneStr(fileName);
-}
-
-FileInfo::~FileInfo(){
-	free(_name);
-	if (_file!=NULL){
-		fclose(_file);
-		_file=NULL;
+	FileInfo::~FileInfo(){
+		free(_name);
+		if (_file!=NULL){
+			FileManager::Close(this);
+			_file=NULL;
+		}
 	}
-}
-
-void FileInfo::close(void){
-	if (_file!=NULL){
-		FileManager::Close(_file);
-		_file=NULL;
+	
+	void FileInfo::close(void) throw (IOException){
+		if (_file!=NULL){
+			FileManager::Close(this);
+			_file=NULL;
+		}
 	}
-}
-
-void FileInfo::open(char* mode){
-	_file=FileManager::Open(_name,mode);
-}
-
-int FileInfo::read(void* place, int size){
-	return FileManager::Read(place,size,_file);	
-}
-
-void FileInfo::create(char* mode){
-	FileManager::Open(_name,mode);
-}
-
-void FileInfo::delet(void){
-	if (_file!=NULL){
-		FileManager::Close(_file);
-		_file=NULL;
+	
+	void FileInfo::open(void) throw (IOException){
+		FileManager::Open(this,"r+b");
+		if (_file == NULL){
+			throw IOException();
+		}
 	}
-	FileManager::Delete(_name);
-}
-
-void FileInfo::write(void* source, int size){
-	FileManager::Write(source,size,_file);
+	
+	int FileInfo::read(void* place, int size) throw (IOException){
+		if (_file == NULL){
+			throw IOException();
+		}
+		return FileManager::Read(place,size,this);	
+	}
+	
+	void FileInfo::create(void) throw (IOException){
+		FileManager::Open(this,"w+b");
+		if (_file == NULL){
+			throw IOException();
+		}
+	}
+	
+	void FileInfo::delet(void) throw (IOException){
+		if (_file!=NULL){
+			FileManager::Close(this);
+			_file=NULL;
+		}
+		FileManager::Delete(this);
+	}
+	
+	void FileInfo::write(void* source, int size) throw (IOException){
+		if (_file == NULL){
+			throw IOException();
+		}
+		FileManager::Write(source,size,this);
+	}
 }
