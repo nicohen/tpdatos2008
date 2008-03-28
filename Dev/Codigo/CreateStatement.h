b117ed2b0e2c467b5e859a4251bc8f475903a3b0
NahuelQuevedo <NahuelQuevedo@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-03-28 22:56:41 +0000

    Se agregaron dos clases vacias: SecondaryIndex y CreateStatement Se agregó un main_nahuel.cpp Se modificó el makefile para que sea mas facil tener multiples "main" y poder switchear facilmente entre cada uno. Solo es cuerstion de modificar la variable MAIN_CLASS

---
+++ Dev/Codigo/CreateStatement.h
@@ -0,0 +1,22 @@
+#ifndef CREATESTATEMENT_H_
+#define CREATESTATEMENT_H_
+
+class CreateStatement
+{
+public:
+	CreateStatement();
+	char* getFileName();
+	int[] getSecondaryFields();
+	int getFileType();
+	int getDataBlockSize();
+	int getIndexSize();
+	
+	SecondaryIndex* getSecondaryIndex();	
+	
+	virtual ~CreateStatement();
+};
+
+
+
+
+#endif /*CREATESTATEMENT_H_*/
