3616645d3ef224ddbd8438f7222578a1fdf5b4d6
pablonazareno <pablonazareno@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-03-29 03:43:13 +0000

    correciones en el makefile

--- Dev/Codigo/main_nahuel.cpp
+++ Dev/Codigo/main_nahuel.cpp
@@ -3,9 +3,9 @@
 #import "stdlib.h"
 #import "SecondaryIndex.h"
 
-/*int main(int argc, char **argv) {
+int main(int argc, char **argv) {
 	SecondaryIndex* vvSecondaryIndex=NULL;
 	vvSecondaryIndex= new SecondaryIndex();
 	printf("Hola desde el main de nahuel");	
 	return 0;
-}*/
+}
