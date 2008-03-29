9db523679fafbdc821aac5fd896df9d7a95df226
NahuelQuevedo <NahuelQuevedo@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-03-29 17:35:10 +0000

    Se terminó la interfaz del create statement

--- Dev/Codigo/CreateStatement.cpp
+++ Dev/Codigo/CreateStatement.cpp
@@ -1,9 +1,35 @@
 #include "CreateStatement.h"
 
+using namespace std;
+
 CreateStatement::CreateStatement()
 {
 }
 
+char* CreateStatement::getFileName(){
+	return NULL;	
+}
+
+int CreateStatement::getDataBlockSize(){
+	return 0;
+}
+
+int CreateStatement::getIndexSize(){
+	return 0;	
+}
+
+SecondaryIndex* CreateStatement::getSecondaryIndex(){
+	return NULL;	
+}
+
+vector<Field*>* CreateStatement::getSecondaryFields(){
+	vector<Field*>* myVec=NULL;
+	myVec=new vector<Field*>();
+	//Usa el constructor de copia de la clase vector
+	return new vector<Field*>(*myVec);
+}
+
 CreateStatement::~CreateStatement()
 {
+	//ToDo: RECORRER Y MATAR CADA UNO DE LOS ELEMENTOS DEL ARRAY
 }
