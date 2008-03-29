dcbe11f4d8a8f91ef3bf3165accb2272fe168217
NahuelQuevedo <NahuelQuevedo@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-03-29 21:09:42 +0000

    Algunas modificaciones el CreateStatement:Tiene casi todas sus propiedades

--- Dev/Codigo/CreateStatement.cpp
+++ Dev/Codigo/CreateStatement.cpp
@@ -1,40 +1,78 @@
 #include "CreateStatement.h"
 #include "Utils.h"
+#include <stdio.h>
 
 using namespace std;
 
 CreateStatement::CreateStatement()
 {
+	_secondaryFields=new vector<Field*>();
 }
 
+char* CreateStatement::getFileName(){
+	return _name;	
+}
+
+int CreateStatement::getDataBlockSize(){
+	return _dataBlockSize;
+}
+
+int CreateStatement::getIndexSize(){
+	return _indexSize;	
+}
+
+SecondaryIndex* CreateStatement::getSecondaryIndex(){
+	return _secondaryIndex;
+}
+
+vector<Field*>* CreateStatement::getSecondaryFields(){
+	//Usa el constructor de copia de la clase vector
+	return new vector<Field*>(*_secondaryFields);
+}
+
+void CreateStatement::addSecondaryField(Field* aField){
+	_secondaryFields->push_back(aField);
+}
+
+void CreateStatement::setSecondaryIndex(SecondaryIndex* aSecondaryIndex){
+	_secondaryIndex=aSecondaryIndex;
+}
+
+void CreateStatement::setDataBlockSize(int size){
+	_dataBlockSize=size;
+}
+void CreateStatement::setIndexSize(int size){
+	_indexSize=size;
+}
+
+/*
+ * El parametro aName será clonado
+ * */
 void CreateStatement::setFileName(char* aName){
 	_name=cloneStr(aName);
 }
 
-char* CreateStatement::getFileName(){
-	return NULL;	
-}
 
-int CreateStatement::getDataBlockSize(){
-	return 0;
-}
-
-int CreateStatement::getIndexSize(){
-	return 0;	
-}
-
-SecondaryIndex* CreateStatement::getSecondaryIndex(){
-	return NULL;	
-}
-
-vector<Field*>* CreateStatement::getSecondaryFields(){
-	vector<Field*>* myVec=NULL;
-	myVec=new vector<Field*>();
-	//Usa el constructor de copia de la clase vector
-	return new vector<Field*>(*myVec);
+void cleanSecondaryFields(vector<Field*>* secondaryFields){
+	vector<Field*>::iterator iter;
+	for (iter = secondaryFields->begin(); iter != secondaryFields->end(); iter++ )
+	{
+		printf("is mandatory: %i \n",((Field*)*iter)->isMandatory());
+		delete ((Field*)*iter);
+	}
+	secondaryFields->clear();
+/*for (Iter = myVector.begin(); Iter != myVector.end(); Iter++ )
+ {
+      cout << " " << *Iter;
+ }*/
 }
 
 CreateStatement::~CreateStatement()
 {
 	//ToDo: RECORRER Y MATAR CADA UNO DE LOS ELEMENTOS DEL ARRAY
+	free(this->_name);
+	delete(this->_secondaryIndex);	
+	cleanSecondaryFields(this->_secondaryFields);
+	delete(this->_secondaryFields);
+	//this->_secondaryFields
 }
