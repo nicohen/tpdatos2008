a8dc153faf1614d2f3e3b1879cd365a91ea72f3d
NahuelQuevedo <NahuelQuevedo@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-03-30 20:29:53 +0000

    Se modificaron algunas cuestiones de implementacion de CreateStatement

--- Dev/Codigo/Field.h
+++ Dev/Codigo/Field.h
@@ -2,14 +2,25 @@
 #define FIELD_H_
 
 #include "DataType.h"
+#include <stdio.h>
 class Field
 {
+private:
+	bool _isMandatory;
+	bool _isPolyvalent;
+	DataType* _dataType;
 public:
 	Field();
-	
+	//Getters
 	bool isMandatory();
 	bool isPolyvalent();
-	DataType* getType();
+	DataType* getDataType();
+	
+	//Setters
+	void setIsMandatory(bool isMandatory);
+	void setIsPolyvalent(bool isPolyvalent);
+	void setDataType(DataType* aDataType);
+	
 	virtual ~Field();
 };
 
