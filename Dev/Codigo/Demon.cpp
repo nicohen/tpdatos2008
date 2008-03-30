85355df15e901f90d0be36c7641d6661e32acefa
pablonazareno <pablonazareno@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-03-30 22:26:27 +0000

    Fin de implementacion de Token y Tokenizer

--- Dev/Codigo/Demon.cpp
+++ Dev/Codigo/Demon.cpp
@@ -15,20 +15,22 @@ Demon::Demon(int sleepTime, int bufferSize, bool vervose){
 }
 
 void Demon::run(void){
+	char SEPARATORS[]= {' ','!','\n'};
+ 	char* KEYW[]= {"Hello","CONSULTAR"};
 	FileManager::FileManager* fileManager = new FileManager::FileManager();
 	FileManager::FileInfo *input;
 	Parsing::Tokenizer *tokenizer;
 	Parsing::Token *token;
 	for(int i=0;i<10;i++){
-		input = fileManager->CreateFileInfo("In/pablo.txt");
+		input = fileManager->CreateFileInfo("In/Comandos.7506");
 		try{
-			tokenizer = new Parsing::Tokenizer(input," ",1);
+			tokenizer = new Parsing::Tokenizer(input, SEPARATORS ,3, KEYW ,2);
 				token = tokenizer->getNextToken(false);
 				while (token!=NULL){
-					printf(token->getContent());
+					printf("(%d)%s ",token->getType(),token->getContent());
 					delete(token);
 					token = tokenizer->getNextToken(false);
-				}	
+				}
 			delete(tokenizer);
 		}catch(FileManager::IOException e){
 			printf("file not found!\n");
