ea74800697563012ff0179f7bbfefc1f74b6ff51
nahuelquevedo <nahuelquevedo@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-04-12 23:56:28 +0000

    BlockStructuredFile: -Refactorizacion interna

--- Dev/Codigo/Data/BlockStructuredFile.h
+++ Dev/Codigo/Data/BlockStructuredFile.h
@@ -32,16 +32,17 @@ private:
 	T_BLOCKCOUNT getAbsoluteBlockNumberFromContentBlockNumber(T_BLOCKCOUNT contentBlockNumber);
 	T_BLOCKCOUNT getHeaderBlockCount();
 	T_FILESIZE  getBlockFilePositionFromAbsoluteBlockCount(T_BLOCKCOUNT contentBlockNumber);
-	
+	void updateBlock(T_BLOCKCOUNT blockNumber,char* content);
+	void notifyBlockUpdated(T_BLOCKCOUNT blockNumber);
 public:
 	
-	void updateBlock(T_BLOCKCOUNT blockNumber,char* content);//ESTO ES PRIVADO
 	char* getBlock(T_BLOCKCOUNT blockNumber);//ESTO ES PRIVADO
 
 	static BlockStructuredFile* Load(char* filename);
 	static BlockStructuredFile* Create(char* filename,T_BLOCKSIZE blockSize);
 	
 	char* getContentBlock(T_BLOCKCOUNT contentBlockNumber);	//El resultado es un puntero alocado en memoria dinamica, por lo tanto hay que eliminarlo
+	
 	
 	void updateContentBlock(T_BLOCKCOUNT contentBlockNumber,char* content);
 	void appendBlock(char* content);
