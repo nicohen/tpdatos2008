5f1295b8eb959766f6a73e4d21a284fbb8005661
nahuelquevedo <nahuelquevedo@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-04-12 19:52:58 +0000

    Se modifico lo posbile para que compile. Falta subir la clase Registry

--- trunk/Dev/Codigo/StatementResult.cpp
+++ trunk/Dev/Codigo/StatementResult.cpp
@@ -12,11 +12,11 @@ bool StatementResult::wasOk(){
 	return true;
 }
 
-
+/*
 vector<Registry*>* StatementResult::getFoundRegistries(){
 	return 0;
 }
-
+*/
 void StatementResult::write(OutPutter* outputter){
 	//Escribe el resultado en el outputter	
 }
