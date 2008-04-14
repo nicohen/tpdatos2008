d91900f6597cc872c357ca8d437c1762efd04927
nahuelquevedo <nahuelquevedo@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-04-14 03:06:20 +0000

    Record: -Se modificó la interfaz

--- trunk/Dev/Codigo/Data/Record.h
+++ trunk/Dev/Codigo/Data/Record.h
@@ -1,15 +1,28 @@
 #ifndef RECORD_H_
 #define RECORD_H_
 
+#include "RawRecord.h"
+#include "../DataValue.h"
+#include <vector>
+using namespace std;
 /*
  * Contiene los datos de un registro
  * */
 class Record
 {
+private:
+	void clearValues(vector<DataValue*>* values);
+	vector<DataValue*>* _values;
 public:
 	Record();
 	virtual ~Record();
 	char* toString();
+	//Getters
+	vector<DataValue*>* getValues();
+	
+	//Otros metodos
+	void addValue(DataValue* aValue);
+	void addValues(vector<DataValue*>* valueList);
+	RawRecord* serialize();
 };
-
 #endif /*RECORD_H_*/
