3cbc76b905b3731ca947f4892b0658cb8ebd5dc1
nahuelquevedo <nahuelquevedo@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-04-14 03:06:20 +0000

    Record: -Se modificó la interfaz

--- Dev/Codigo/Data/Record.cpp
+++ Dev/Codigo/Data/Record.cpp
@@ -1,9 +1,36 @@
 #include "Record.h"
-
+using namespace std;
 Record::Record()
 {
 }
 
 Record::~Record()
 {
+	clearValues(_values);
+	delete _values;
+}
+
+vector<DataValue*>* Record::getValues(){
+	//Usa el constructor de copia de la clase vector
+	return new vector<DataValue*>(*_values);
+}
+
+void Record::addValue(DataValue* aValue){
+	_values->push_back(aValue);
+}
+
+void Record::addValues(std::vector<DataValue*>* valueList){
+	vector<DataValue*>::iterator iter;
+	for (iter = valueList->begin(); iter != valueList->end(); iter++ ){
+		_values->push_back(*iter);
+	}
+	delete valueList;
+}
+
+void Record::clearValues(vector<DataValue*>* values){
+	vector<DataValue*>::iterator iter;
+	for (iter = _values->begin(); iter != _values->end(); iter++ ) {
+		delete ((DataValue*)*iter);
+	}
+	values->clear();
 }
