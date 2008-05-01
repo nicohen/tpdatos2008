a42baed5f2bab54653bc200dd9076aea2072a160
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-05-01 19:09:55 +0000

    Agrego guión en la salida al Out del RemoveStatement Creo Algoritmo de seleccion de elemento a reemplazar en el buffer

---
+++ trunk/Dev/Codigo/Buffer/ReplacementSelector.h
@@ -0,0 +1,28 @@
+#ifndef REPLACEMENTSELECTOR_H_
+#define REPLACEMENTSELECTOR_H_
+#include <iostream.h>
+#include <map>
+#include "IComparable.h"
+
+using namespace std;
+
+struct strCmp {
+	bool operator() (IComparable* i1, IComparable* i2) const {
+		return i1->isLowerThan(i2);
+	}
+};
+
+class ReplacementSelector {
+
+private:
+	map<IComparable*, int, strCmp>* _itemsMap;
+	
+public:
+	ReplacementSelector();
+	virtual ~ReplacementSelector();
+	void setItem(IComparable* key);
+	void notifyHit(IComparable* key);
+	IComparable* getUnusedItem();	
+};
+
+#endif /*REPLACEMENTSELECTOR_H_*/
