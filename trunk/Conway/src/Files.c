d67614229217f62bfd3f83d0b8508d821f274fd1
pablonazareno <pablonazareno@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-05-13 20:22:34 +0000

    Modulizacion del mensajes Implementacion de salida a PBM

--- trunk/Conway/src/Files.c
+++ trunk/Conway/src/Files.c
@@ -1,17 +1,40 @@
 #include "Files.h"
 #include "Board.h"
 #include <stdio.h>
+#include <string.h>
 
 int setUpBoardFromFile(TDA_Board* board, char* fileName){
-	// TODO: hacer validaciones
 	int x,y;
+	int result=1;
 	FILE* entrada= fopen(fileName,"r");
-	if (entrada!=NULL){
-		while (!feof(entrada)){
-			fscanf(entrada,"%d %d\n",&x,&y);
-			setCharAtPosition(board,x,y,'1');
-		}
-		fclose(entrada);
+	if (entrada==NULL){
+		return 0;
 	}
+	while ((!feof(entrada))&&(result)){
+		fscanf(entrada,"%d %d\n",&x,&y);
+		result=setCharAtPosition(board,x-1,y-1,'1');
+	}
+	fclose(entrada);
+	return result;
+}
+
+int createPBMFromBoad(TDA_Board* board, char* prefix, int iteration){
+	int i,j;
+	char buffer[50];
+	FILE* salida;
+	sprintf(buffer,"%s_%d.pbm",prefix,iteration);
+	salida=fopen(buffer,"w");
+	if (salida==NULL){
+		return 0;
+	}
+	fprintf(salida,"P1\n");
+	fprintf(salida,"%d %d\n",board->columns,board->rows);
+	for(i=0;i<board->rows;i++){
+		for(j=0;j<board->columns-1;j++){
+			fprintf(salida,"%c",getCharAtPosition(board,i,j));
+		}
+		fprintf(salida,"%c\n",getCharAtPosition(board,i,j));
+	}
+	fclose(salida);
 	return 1;
 }
