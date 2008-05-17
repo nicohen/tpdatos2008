75293291b7d88c359296171d0d3d5a4108a89cd6
pablonazareno <pablonazareno@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-05-17 11:00:15 +0000

    HashTable

---
+++ trunk/Dev/Codigo/Hash/HashTable.cpp
@@ -0,0 +1,49 @@
+#include "HashTable.h"
+#include "stdio.h"
+#include "../Utils.h"
+
+HashTable::HashTable(){
+	_hashFile=NULL;
+}
+
+HashTable::~HashTable(){
+	if (_hashFile!=NULL){
+		fclose(_hashFile);
+	}
+	free(_fileName);
+}
+
+void HashTable::create(char* filePath, int size){
+	_size=size;
+	_hashFile=fopen(filePath,"w+");
+	_fileName= cloneStr(filePath);
+	fwrite(&size,INT_SIZE,1,_hashFile);	
+}
+
+void HashTable::load(char* filePath){
+	_fileName= cloneStr(filePath);
+	_hashFile=fopen(filePath,"r+");
+	fread(&_size,INT_SIZE,1,_hashFile);
+}
+
+int HashTable::getSize(){
+	return _size;
+}
+
+int HashTable::getAt(int i){
+	int content;
+	fseek(_hashFile,(i+1)*INT_SIZE,SEEK_SET);
+	fread(&content,INT_SIZE,1,_hashFile);
+	return content;
+}
+
+void HashTable::update(int index, int value){
+	fseek(_hashFile,(index+1)*INT_SIZE,SEEK_SET);
+	fwrite(&value,INT_SIZE,1,_hashFile);
+}
+
+void HashTable::grow(){
+	_size=_size+_size;
+	fseek(_hashFile,0,SEEK_SET);
+	fwrite(&_size,INT_SIZE,1,_hashFile);
+}
---
+++ trunk/Dev/Codigo/Hash/HashTable.h
@@ -0,0 +1,24 @@
+#ifndef HASHTABLE_H_
+#define HASHTABLE_H_
+
+#import "stdio.h"
+
+class HashTable{
+	private:
+		int _size;
+		FILE* _hashFile;
+		char* _fileName;
+		
+		static const int INT_SIZE= sizeof(int);
+	public:
+		HashTable();
+		virtual ~HashTable();
+		void create(char* filePath, int size);
+		void load(char* filePath);
+		int getSize();
+		int getAt(int i);
+		void update(int index, int value);
+		void grow();
+};
+
+#endif /*HASHTABLE_H_*/
