126ed33a837d2f58a3e721ed1d4cb585bf956394
nahuelquevedo <nahuelquevedo@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-05-18 00:42:32 +0000

    Se actualizo el Eliminar de DataManager

--- trunk/Dev/Codigo/DataManager.cpp
+++ trunk/Dev/Codigo/DataManager.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "Utils.h"
 #include "string.h"
+#include "Data/FileNotFoundException.h"
 
 using namespace std;
 
@@ -35,26 +36,13 @@ DataFile* DataManager::getFile(char* fileName) {
 }
 
 bool DataManager::removeFile(char* fileName) {
-//	string* buffer = new string();
-//	char* cadena;
-//	bool isRemoved = false;
-//	
-//	buffer->append(_basePath);
-//	buffer->append(fileName);
-//	
-//	cadena = (char*) malloc(strlen(buffer->c_str()));
-//	strcpy(cadena,buffer->c_str());
-//	
-//	isRemoved = hadSuccessRemoving(remove(cadena));
-//	
-//	//Eliminar del buffer	
-//	_dataFilesBuffer->removeFile(fileName);
-//	
-//	delete buffer;
-//	free(cadena);
-//	return isRemoved;
-	this->getFile(fileName)->deleTe();
-	return true;
+	try{
+		this->getFile(fileName)->deleTe();
+		return true;	
+	}catch(FileNotFoundException* ex){
+		delete ex;
+		return false;
+	}
 }
 
 char* DataManager::getBasePath() {
