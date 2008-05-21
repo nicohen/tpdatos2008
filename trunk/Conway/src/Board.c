9aacc03420fded29f1619cc0cf7f50911d42b92e
pablonazareno <pablonazareno@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-05-21 05:20:31 +0000

    Implementacion de Vecinos en MIPS Correccion del makefile para que compile en 2 versiones diferentes.

--- trunk/Conway/src/Board.c
+++ trunk/Conway/src/Board.c
@@ -1,47 +1,7 @@
 #include "Board.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-unsigned int vecinos(unsigned char *a, unsigned int i, unsigned int j, unsigned int M, unsigned int N){
-	int x,y;
-	unsigned int activos=0;
-	x=((i-1)<0)?M-1:(i-1);
-	y=((j-1)<0)?N-1:(j-1);
-	if(a[(x*N)+y]=='1'){
-		activos++;
-	}
-	y=j;
-	if(a[(x*N)+y]=='1'){
-		activos++;
-	}
-	y=((j+1)==N)?0:(j+1);
-	if(a[(x*N)+y]=='1'){
-		activos++;
-	}
-	x=i;
-	y=((j-1)<0)?N-1:(j-1);
-	if(a[(x*N)+y]=='1'){
-		activos++;
-	}
-	y=((j+1)==N)?0:(j+1);
-	if(a[(x*N)+y]=='1'){
-		activos++;
-	}
-	x=((i+1)==M)?0:(i+1);
-	y=((j-1)<0)?N-1:(j-1);
-	if(a[(x*N)+y]=='1'){
-		activos++;
-	}
-	y=j;
-	if(a[(x*N)+y]=='1'){
-		activos++;
-	}
-	y=((j+1)==N)?0:(j+1);
-	if(a[(x*N)+y]=='1'){
-		activos++;
-	}
-	return activos;
-}
+#include "Vecinos.h"
 
 TDA_Board* createBoard(int rows,int columns){
 	if ((columns<1)||(rows<1)){
