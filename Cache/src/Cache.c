f85affcea665ef347f2896cace54886f9fa134b9
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-06-18 01:22:48 +0000

    Codigo que esta en el enunciado

---
+++ Cache/src/Cache.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 192
+
+int main(void) {
+	double a[N][N],b[N][N],c[N][N];
+	int i,j,k;
+	
+	for (i=0;i<N;i++) 
+		for (j=0;j<N;i++)
+			for (k=0;k<N;i++)
+				c[i][j] += a[i][k]*b[k][j];
+	
+	return 0;
+}
