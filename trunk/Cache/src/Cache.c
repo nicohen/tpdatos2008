f0c56de4deadaa775efeeb4dc7dcdd049becee13
pablonazareno <pablonazareno@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-06-18 04:08:58 +0000

    resultado de las corridas

--- trunk/Cache/src/Cache.c
+++ trunk/Cache/src/Cache.c
@@ -1,7 +1,7 @@
 #define N 192
 #define B 8
 
-int main(void) {
+int main1(void) {
 	double a[N][N],b[N][N],c[N][N];
 	int i,j,k;
 	
@@ -30,7 +30,7 @@ int main2(void) {
 }
 
 int main3(void) {
-	double a[N][N],pad1[N][1],b[N][N],pad2[N][1],c[N][N];
+	double a[N][N],pad1[8][1],b[N][N],pad2[8][1],c[N][N];
 	int i,j,k;
 	
 	for (i=0;i<N;i++) 
@@ -54,5 +54,24 @@ int main4(void) {
 			for (k=kk;k<kk+B;k++)
 				c[i][j] += a[i][k]*b[k][j];
 	
+	return 0;
+}
+
+int main (void) {
+	volatile double  a[N][N],b[N][N],c[N][N];
+	register int i,j,k,jj,kk;
+	register double temp;
+	
+	for (jj=0;jj<N;jj+=B)
+		for (kk=0;kk<N;kk+=B) 
+			for (i=0; i<N;i++)
+				for (j=jj;j<jj+B;j++){
+			
+				    temp = 0.0;
+				    for (k=kk;k<kk+B;k++) {
+					temp+=a[i][k]*b[k][j];
+				    }
+				    c[i][j] = temp;
+				}
 	return 0;
 }
