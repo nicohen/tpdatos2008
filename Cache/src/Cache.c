f42162ff14ead3565d8fbccfa281af51a1eb202e
pablonazareno <pablonazareno@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-06-18 03:26:09 +0000

    posibles mejoras

--- Cache/src/Cache.c
+++ Cache/src/Cache.c
@@ -1,4 +1,5 @@
 #define N 192
+#define B 8
 
 int main(void) {
 	double a[N][N],b[N][N],c[N][N];
@@ -7,6 +8,50 @@ int main(void) {
 	for (i=0;i<N;i++) 
 		for (j=0;j<N;j++)
 			for (k=0;k<N;k++)
+				c[i][j] += a[i][k]*b[k][j];
+	
+	return 0;
+}
+
+int main2(void) {
+	double a[N][N],b[N][N],c[N][N];
+	int i,j,k;
+	double aux;
+	
+	for (i=0;i<N;i++) 
+		for (j=0;j<N;j++){
+			aux=0;
+			for (k=0;k<N;k++)
+				aux+=a[i][k]*b[k][j];
+			c[i][j]=aux ;
+		}
+	
+	return 0;
+}
+
+int main3(void) {
+	double a[N][N],pad1[N][1],b[N][N],pad2[N][1],c[N][N];
+	int i,j,k;
+	
+	for (i=0;i<N;i++) 
+		for (j=0;j<N;j++)
+			for (k=0;k<N;k++)
+				c[i][j] += a[i][k]*b[k][j];
+	
+	return 0;
+}
+
+
+int main4(void) {
+	double a[N][N],b[N][N],c[N][N];
+	int i,j,k;
+	int jj,kk;
+	
+	for (jj=0;jj<N;jj+=B) 
+	for (kk=0;kk<N;kk+=B)
+	for (i=0;i<N;i++) 
+		for (j=jj;j<jj+B;j++)
+			for (k=kk;k<kk+B;k++)
 				c[i][j] += a[i][k]*b[k][j];
 	
 	return 0;
