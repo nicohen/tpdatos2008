2356e32332f166e91c7b5b6bdec891d44b14bc26
pablonazareno <pablonazareno@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-06-26 02:26:36 +0000

    otro prefetch

--- Cache/src/Cache.c
+++ Cache/src/Cache.c
@@ -2,18 +2,20 @@
 #define B 128
 #define P 8
 #define D 16 
+#define PREFETCH(m,x,y) prefetch = (int) m[x][y];
 
 int mainOriginal(void) {
 	double a[N][N],b[N][N],c[N][N];
 	int i,j,k;
+	register int prefetch;
 	
 	for (i=0;i<N;i++){ 
 		for (j=0;j<N;j++){
 			for (k=0;k<D;k+=8) {
-				__builtin_prefetch(&a[i][k]);
+				PREFETCH(a,i,k);
 			}
 			for (k=0;k<N-D;k+=8) {
-				__builtin_prefetch(&a[i][k+D]);
+				PREFETCH(a,i,k+D);
 				c[i][j] += a[i][k]*b[k][j];
 				c[i][j] += a[i][k+1]*b[k+1][j];
 				c[i][j] += a[i][k+2]*b[k+2][j];
@@ -89,37 +91,38 @@ int main5 (void) {
 int mainDoblePrefetch(void) {
 	double a[N][N],b[N][N],c[N][N];
 	int i,j,k;
+	register int prefetch;
 	
 	for (i=0;i<N;i++){ 
 		for (j=0;j<N;j++){
 			for (k=0;k<D;k+=8) {
-				__builtin_prefetch(&a[i][k]);
-				__builtin_prefetch(&b[i][k]);
-				__builtin_prefetch(&b[i][k+1]);
-				__builtin_prefetch(&b[i][k+2]);
-				__builtin_prefetch(&b[i][k+3]);
-				__builtin_prefetch(&b[i][k+4]);
-				__builtin_prefetch(&b[i][k+5]);
-				__builtin_prefetch(&b[i][k+6]);
-				__builtin_prefetch(&b[i][k+7]);
+				PREFETCH(a,i,k);
+				PREFETCH(b,i,k);
+				PREFETCH(b,i,k+1);
+				PREFETCH(b,i,k+2);
+				PREFETCH(b,i,k+3);
+				PREFETCH(b,i,k+4);
+				PREFETCH(b,i,k+5);
+				PREFETCH(b,i,k+6);
+				PREFETCH(b,i,k+7);
 			}
 			for (k=0;k<N-D;k+=8) {
-				__builtin_prefetch(&a[i][k+D]);
-				__builtin_prefetch(&b[i][k+D]);
+				PREFETCH(a,i,k+D);
+				PREFETCH(b,i,k+D);
 				c[i][j] += a[i][k]*b[k][j];
-				__builtin_prefetch(&b[i][k+D+1]);
+				PREFETCH(b,i,k+D+1);
 				c[i][j] += a[i][k+1]*b[k+1][j];
-				__builtin_prefetch(&b[i][k+D+2]);
+				PREFETCH(b,i,k+D+2);
 				c[i][j] += a[i][k+2]*b[k+2][j];
-				__builtin_prefetch(&b[i][k+D+3]);
+				PREFETCH(b,i,k+D+3);
 				c[i][j] += a[i][k+3]*b[k+3][j];
-				__builtin_prefetch(&b[i][k+D+4]);
+				PREFETCH(b,i,k+D+4);
 				c[i][j] += a[i][k+4]*b[k+4][j];
-				__builtin_prefetch(&b[i][k+D+5]);
+				PREFETCH(b,i,k+D+5);
 				c[i][j] += a[i][k+5]*b[k+5][j];
-				__builtin_prefetch(&b[i][k+D+6]);
+				PREFETCH(b,i,k+D+6);
 				c[i][j] += a[i][k+6]*b[k+6][j];
-				__builtin_prefetch(&b[i][k+D+7]);
+				PREFETCH(b,i,k+D+7);
 				c[i][j] += a[i][k+7]*b[k+7][j];
 			}
 			for (k=N-D;k<N;k+=8) {
@@ -141,38 +144,39 @@ int mainDoblePrefetch(void) {
 int mainTriplePrefetch(void) {
 	double a[N][N],b[N][N],c[N][N];
 	int i,j,k;
+	register int prefetch;
 	
 	for (i=0;i<N;i++){ 
 		for (j=0;j<N;j++){
-			__builtin_prefetch(&c[i][j]);
+			PREFETCH(c,i,j);
 			for (k=0;k<D;k+=8) {
-				__builtin_prefetch(&a[i][k]);
-				__builtin_prefetch(&b[i][k]);
-				__builtin_prefetch(&b[i][k+1]);
-				__builtin_prefetch(&b[i][k+2]);
-				__builtin_prefetch(&b[i][k+3]);
-				__builtin_prefetch(&b[i][k+4]);
-				__builtin_prefetch(&b[i][k+5]);
-				__builtin_prefetch(&b[i][k+6]);
-				__builtin_prefetch(&b[i][k+7]);
+				PREFETCH(a,i,k);
+				PREFETCH(b,i,k);
+				PREFETCH(b,i,k+1);
+				PREFETCH(b,i,k+2);
+				PREFETCH(b,i,k+3);
+				PREFETCH(b,i,k+4);
+				PREFETCH(b,i,k+5);
+				PREFETCH(b,i,k+6);
+				PREFETCH(b,i,k+7);
 			}
 			for (k=0;k<N-D;k+=8) {
-				__builtin_prefetch(&a[i][k+D]);
-				__builtin_prefetch(&b[i][k+D]);
+				PREFETCH(a,i,k+D);
+				PREFETCH(b,i,k+D);
 				c[i][j] += a[i][k]*b[k][j];
-				__builtin_prefetch(&b[i][k+D+1]);
+				PREFETCH(b,i,k+D+1);
 				c[i][j] += a[i][k+1]*b[k+1][j];
-				__builtin_prefetch(&b[i][k+D+2]);
+				PREFETCH(b,i,k+D+2);
 				c[i][j] += a[i][k+2]*b[k+2][j];
-				__builtin_prefetch(&b[i][k+D+3]);
+				PREFETCH(b,i,k+D+3);
 				c[i][j] += a[i][k+3]*b[k+3][j];
-				__builtin_prefetch(&b[i][k+D+4]);
+				PREFETCH(b,i,k+D+4);
 				c[i][j] += a[i][k+4]*b[k+4][j];
-				__builtin_prefetch(&b[i][k+D+5]);
+				PREFETCH(b,i,k+D+5);
 				c[i][j] += a[i][k+5]*b[k+5][j];
-				__builtin_prefetch(&b[i][k+D+6]);
+				PREFETCH(b,i,k+D+6);
 				c[i][j] += a[i][k+6]*b[k+6][j];
-				__builtin_prefetch(&b[i][k+D+7]);
+				PREFETCH(b,i,k+D+7);
 				c[i][j] += a[i][k+7]*b[k+7][j];
 			}
 			for (k=N-D;k<N;k+=8) {
