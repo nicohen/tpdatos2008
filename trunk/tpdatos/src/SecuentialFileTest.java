7ecc08fb410939ddded74495f54ec186b164942d
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-08-27 21:42:03 +0000

    Agrego mensajes en las excepciones Se crea Junit test para SecuentialFileTest.java

---
+++ trunk/tpdatos/src/SecuentialFileTest.java
@@ -0,0 +1,44 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import persistors.BytePersistor;
+import files.File;
+import files.SecuencialFile;
+import junit.framework.TestCase;
+
+
+public class SecuentialFileTest extends TestCase {
+	public SecuentialFileTest() {
+		
+	}
+
+	public void testMe() {
+		BytePersistor persistor= new BytePersistor();
+		try {
+			File<Byte> file= new SecuencialFile<Byte>("c:/datos.bin",persistor);
+			file.add(new Byte((byte)7));
+			file.add(new Byte((byte)8));
+			file.add(new Byte((byte)9));
+			file.add(new Byte((byte)10));
+			
+			Byte aux= file.get(0);
+			System.out.println(aux);
+			assertEquals(aux, new Byte((byte)7));
+			aux= file.get(3);
+			System.out.println(aux);
+			assertEquals(aux, new Byte((byte)10));
+			aux= file.get(2);
+			System.out.println(aux);
+			assertEquals(aux, new Byte((byte)9));
+			aux= file.get(1);
+			System.out.println(aux);
+			assertEquals(aux, new Byte((byte)8));
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
--- trunk/tpdatos/src/exceptions/BusinessException.java
+++ trunk/tpdatos/src/exceptions/BusinessException.java
@@ -4,12 +4,16 @@ public class BusinessException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 	
+	public BusinessException() {
+		super();
+	}
+	
 	public BusinessException(String reason) {
-		
+		super(reason);
 	}
 
 	public BusinessException(String reason, Throwable cause) {
-		
+		super(reason,cause);
 	}
 
 }
