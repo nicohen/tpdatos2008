011d6a6c5009099dcf77d8a39348b7ffb561b0df
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-08-27 21:42:03 +0000

    Agrego mensajes en las excepciones Se crea Junit test para SecuentialFileTest.java

--- tpdatos/src/exceptions/DataAccessException.java
+++ tpdatos/src/exceptions/DataAccessException.java
@@ -4,12 +4,16 @@ public class DataAccessException extends Exception {
 	
 	private static final long serialVersionUID = 1L;
 	
+	public DataAccessException() {
+		super();
+	}
+	
 	public DataAccessException(String reason) {
-		
+		super(reason);
 	}
 
 	public DataAccessException(String reason, Throwable cause) {
-		
+		super(reason,cause);
 	}
 
 }
