98704f09c07da3494cac3fdd520de79d1bbcd1a8
pablonazareno <pablonazareno@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-11 14:29:43 +0000

    renombre el metodo modificar por update en la interface File

--- trunk/tpdatos/src/files/File.java
+++ trunk/tpdatos/src/files/File.java
@@ -6,7 +6,7 @@ public interface File<E>{
 	
 	public int add(E element) throws DataAccessException;
 	public E get(int elementId) throws DataAccessException;
-	public int modify(int elementId, E newElement) throws DataAccessException;
+	public int update(int elementId, E newElement) throws DataAccessException;
 	public void remove(int elementId) throws DataAccessException;
 	public void delete();
 }
