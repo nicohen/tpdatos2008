79bdaf20e01c139c5d0fec0a526b8e2039e1fd09
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-12 02:53:57 +0000

    implementado WordsProcessor de stopwords

--- trunk/tpdatos/src/processor/words/machine/StopwordsWordsProcessor.java
+++ trunk/tpdatos/src/processor/words/machine/StopwordsWordsProcessor.java
@@ -1,16 +1,27 @@
 package processor.words.machine;
 
+import java.util.HashSet;
+
 import dto.WordDto;
 import processor.words.AbstractWordsProcessor;
 
 public class StopwordsWordsProcessor extends AbstractWordsProcessor {
 
+	private HashSet<String> set;
+	
 	public StopwordsWordsProcessor() {
 		super();
 	}
 	
+	void addStopword( String word ) {
+		set.add(word);
+	}
+	
 	@Override
 	public void process(WordDto wordDto) {
+		if ( ! set.contains(wordDto.toString() )) {
+			super.process(wordDto);
+		}
 	}
 
 }
