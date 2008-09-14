a49f8b01caac94a295fad7eed3d04c78f0b8733b
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-14 19:07:42 +0000

    refactoring de DocumentIndexKey y de BPlusTree por BPlusTreeFacade

---
+++ trunk/tpdatos/src/bplus/BPlusTreeFacade.java
@@ -0,0 +1,27 @@
+package bplus;
+
+import documents.keys.DocumentIndexKey;
+import dto.WordDto;
+
+public class BPlusTreeFacade {
+
+/*	public void insertNode(BPlusNode node) {
+		//TODO - Implementar insertNode
+	}
+	
+	public void getNode(Integer id) {
+		//TODO - Implementar getNode
+	}*/
+	
+	public DocumentIndexKey getElement( WordDto key ) {
+		
+		// TODO
+		return new DocumentIndexKey( 0 );
+	}
+	
+	public void insertElement (WordDto key , Integer value ) {
+		// TODO
+	}
+	
+	
+}
