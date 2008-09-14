5876443786f84e07eeb0188a9ea446aba3a0e24a
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-14 20:34:03 +0000

    Agregado nodeKey al BPlusIndex

--- trunk/tpdatos/src/bplus/nodes/BPlusIndexNode.java
+++ trunk/tpdatos/src/bplus/nodes/BPlusIndexNode.java
@@ -1,6 +1,13 @@
 package bplus.nodes;
 
+import bplus.keys.BPlusNodeKey;
+
 
 public class BPlusIndexNode extends AbstractBPlusNode {
+
+	public BPlusIndexNode(BPlusNodeKey nodeKey) {
+		super(nodeKey);
+		// TODO Auto-generated constructor stub
+	}
 
 }
