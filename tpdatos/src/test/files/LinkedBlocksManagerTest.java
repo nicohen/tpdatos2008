014ecf6e012d9c44fc6274f532878932ade8dcf6
berta1108 <berta1108@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-15 19:44:12 +0000

    mods varias

--- tpdatos/src/test/files/LinkedBlocksManagerTest.java
+++ tpdatos/src/test/files/LinkedBlocksManagerTest.java
@@ -1,6 +1,9 @@
 package test.files;
+import java.util.ArrayList;
+
 import dto.LinkedBlock;
 import junit.framework.TestCase;
+import api.LinkedBlocksManager;
 import api.po.files.File;
 import app.po.files.RelativeFile;
 import app.po.persistors.LinkedBlockIntPersistor;
@@ -10,7 +13,7 @@ import exceptions.DataAccessException;
 public class LinkedBlocksManagerTest extends TestCase {
 
 	// FIXME: no rethrowear mejor hacer algo con el junit ?
-	public void testLinkedBlocksPersistor() throws DataAccessException {
+/*	public void testLinkedBlocksPersistor() throws DataAccessException {
 		LinkedBlockIntPersistor persistor=new LinkedBlockIntPersistor(24);
 		File<LinkedBlock<Integer>> file= new RelativeFile<LinkedBlock<Integer>>("test_relative_block.bin",persistor);
 		LinkedBlock<Integer> reg=new LinkedBlock<Integer>();
@@ -48,7 +51,23 @@ public class LinkedBlocksManagerTest extends TestCase {
 		LinkedBlock<Integer> aux4= file.get(3);
 		//assertEquals(aux, reg);
 		
+	}*/
+	public void testLinkedBlocksManager() throws DataAccessException {
+		LinkedBlockIntPersistor persistor=new LinkedBlockIntPersistor(24);
+		LinkedBlocksManager<Integer> manager=new LinkedBlocksManager<Integer>("test_relative_block.bin",5 , persistor);
+		ArrayList<Integer> aux;
+		manager.add(1, 0);
+		manager.add(2, 0);
+		manager.add(3, 0);
+		manager.add(4, 0);
+		manager.add(5, 0);
+		manager.add(6, 0);
+		manager.add(7, 0);
+		manager.add(8, 0);
+		aux=manager.get(0);
+		
+		//assertEquals(aux, reg);
+		
 	}
-	
 	
 }
