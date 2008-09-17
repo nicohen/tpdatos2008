5409cbd2356f36823eb3ab36b44c6b6003ca5420
berta1108 <berta1108@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-17 15:24:41 +0000

    Para gamma y delta encoding se van a usar byte arrays

--- trunk/tpdatos/src/test/files/LinkedBlocksManagerTest.java
+++ trunk/tpdatos/src/test/files/LinkedBlocksManagerTest.java
@@ -41,7 +41,7 @@ public class LinkedBlocksManagerTest extends TestCase {
 		//deberia ser: 1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16
 		aux=manager.get(5);
 		//deberia ser: 17-18-19-20-21-22-23
-		//assertEquals(aux, reg);
+		//assertEquals(aux, a);
 		
 	}
 	
--- trunk/tpdatos/src/utils/KeyCodificationUtils.java
+++ trunk/tpdatos/src/utils/KeyCodificationUtils.java
@@ -2,32 +2,32 @@ package utils;
 
 public class KeyCodificationUtils {
 
-	public static Integer unarioEncode(Integer key) { 
+	public static Byte[] unarioEncode(Integer key) { 
 		//TODO - Implementar unarioEncode
 		return null; 
 	}
 	
-	public static Integer unarioDecode(Integer key) { 
+	public static Integer unarioDecode(Byte[] key) { 
 		//TODO - Implementar unarioDecode
 		return null; 
 	}
 	
-	public static Integer gammaEncode(Integer key) { 
+	public static Byte[] gammaEncode(Integer key) { 
 		//TODO - Implementar gammaEncode
 		return null; 
 	}
 	
-	public static Integer gammaDecode(Integer key) { 
+	public static Integer gammaDecode(Byte[] key) { 
 		//TODO - Implementar gammaDecode
 		return null; 
 	}
 	
-	public static Integer deltaEncode(Integer key) { 
+	public static Byte[] deltaEncode(Integer key) { 
 		//TODO - Implementar deltaEncode
 		return null; 
 	}
 	
-	public static Integer deltaDecode(Integer key) { 
+	public static Integer deltaDecode(Byte[] key) { 
 		//TODO - Implementar deltaDecode
 		return null; 
 	}
