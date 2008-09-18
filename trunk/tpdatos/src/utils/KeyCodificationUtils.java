871a427b8c77fe9949e9a04d2bad9cde3afc4ecb
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-18 05:04:59 +0000

    Simplifico la logica para el wordsprocessor Creo unarioEncode Creo unarioDecode Creo gammaEncode Creo JUnit para gammaEncode

--- trunk/tpdatos/src/utils/KeyCodificationUtils.java
+++ trunk/tpdatos/src/utils/KeyCodificationUtils.java
@@ -2,19 +2,45 @@ package utils;
 
 public class KeyCodificationUtils {
 
-	public static Byte[] unarioEncode(Integer key) { 
-		//TODO - Implementar unarioEncode
-		return null; 
+	public static Byte[] unarioEncode(String key) {
+		Integer longitud = key.length();
+		Byte[] unario = new Byte[longitud];
+		for (int i=0;i<longitud-1;i++) {
+			unario[i] = 0;
+		}
+		unario[longitud]=1;
+		return unario;
 	}
 	
-	public static Integer unarioDecode(Byte[] key) { 
-		//TODO - Implementar unarioDecode
-		return null; 
+	public static String unarioDecode(Byte[] key) {
+		return String.valueOf(key.length);
 	}
 	
 	public static Byte[] gammaEncode(Integer key) { 
-		//TODO - Implementar gammaEncode
-		return null; 
+		Byte[] gamma = null;
+		if (key==1) {
+			gamma = new Byte[1];
+			gamma[0]=48;
+			return gamma;
+		}
+		String gammaStr = "";
+		String logaritmoEnBase2DeN = String.valueOf((int)Math.floor(Math.log(key)/Math.log(2)));
+		for (int i=0;i<Integer.parseInt(logaritmoEnBase2DeN);i++) {
+			gammaStr += 1;	
+		}
+		gammaStr += 0;
+		String ultimos = String.valueOf((key-((int)Math.pow(2,Integer.parseInt(logaritmoEnBase2DeN)))));
+		for (int i=0;i<Integer.parseInt(logaritmoEnBase2DeN)-ultimos.length();i++) {
+			gammaStr+=0;
+		}
+		for (int i=0;i<ultimos.length();i++) {
+			gammaStr+=ultimos.charAt(i);
+		}
+		gamma = new Byte[gammaStr.length()];
+		for (int i=0;i<gammaStr.length();i++) {
+			gamma[i]=(byte)gammaStr.charAt(i);
+		}
+		return gamma;
 	}
 	
 	public static Integer gammaDecode(Byte[] key) { 
@@ -22,14 +48,14 @@ public class KeyCodificationUtils {
 		return null; 
 	}
 	
-	public static Byte[] deltaEncode(Integer key) { 
-		//TODO - Implementar deltaEncode
-		return null; 
-	}
-	
-	public static Integer deltaDecode(Byte[] key) { 
-		//TODO - Implementar deltaDecode
-		return null; 
-	}
+//	public static Byte[] deltaEncode(Integer key) { 
+//		//TODO - Implementar deltaEncode
+//		return null; 
+//	}
+//	
+//	public static Integer deltaDecode(Byte[] key) { 
+//		//TODO - Implementar deltaDecode
+//		return null; 
+//	}
 	
 }
