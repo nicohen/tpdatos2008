1387bb054647d353e1e402b30382f8e8f4198087
pablonazareno <pablonazareno@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-21 16:44:41 +0000

    Se borra la clase BPlusKey

--- trunk/tpdatos/src/bplus/keys/BPlusKey.java
+++
@@ -1,8 +0,0 @@
-package bplus.keys;
-
-public abstract class BPlusKey<E>  implements Comparable<BPlusKey>{
-	public abstract E getValue();
-	public abstract void setValue(E element);
-	public abstract int getSize();
-	public abstract int compareTo(BPlusKey element);
-}
