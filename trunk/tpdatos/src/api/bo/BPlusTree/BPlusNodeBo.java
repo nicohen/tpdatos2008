be57dd8846007d65114ff594aab88e22a730f004
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-21 15:18:04 +0000

    Definidos metodos de la interfaz del BPlusNodeBo

--- trunk/tpdatos/src/api/bo/BPlusTree/BPlusNodeBo.java
+++ trunk/tpdatos/src/api/bo/BPlusTree/BPlusNodeBo.java
@@ -1,11 +1,19 @@
 package api.bo.BPlusTree;
 
-import bplus.keys.BPlusNodeKey;
-import bplus.nodes.BPlusNode;
-import exceptions.DataAccessException;
+import bplus.elements.BPlusLeafElement;
+import bplus.exceptions.KeyNotFoundException;
+import bplus.keys.BPlusElementKey;
+
+// EL BO encapsula la logica de interaccion con nodos, index set, secuencial set, etc...
+// por lo tanto solo provee metodo para insertar ,eliminar, actualizar y recuperar elementos de hojas
 
 public interface BPlusNodeBo {
-	public abstract void updateNode(BPlusNode node) throws DataAccessException;
+	/*public abstract void updateNode(BPlusNode node) throws DataAccessException;
 	public abstract BPlusNodeKey insertNode(BPlusNode node) throws DataAccessException;
-	public abstract BPlusNode getNode(BPlusNodeKey id) throws DataAccessException;
+	public abstract BPlusNode getNode(BPlusNodeKey id) throws DataAccessException;*/
+
+	public void insertElement( BPlusLeafElement element ) ;
+	public void removeElement( BPlusElementKey elementKey ) throws KeyNotFoundException ;
+	public void updateElement( BPlusLeafElement element ) throws KeyNotFoundException ;
+	public BPlusLeafElement getElement( BPlusElementKey elementKey) throws KeyNotFoundException ; 
 }
