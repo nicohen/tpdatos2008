ad738d4b273255fe332b9cc40475d004b04189d7
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-21 18:02:25 +0000

    Modificado BPlusTreeBoTest para usar String como clave del map

--- tpdatos/src/test/bplus/MapTest.java
+++ tpdatos/src/test/bplus/MapTest.java
@@ -3,21 +3,20 @@ package test.bplus;
 import java.util.HashMap;
 import java.util.Map;
 
+import junit.framework.TestCase;
 import bplus.elements.BPlusLeafElement;
 import bplus.keys.BPlusElementKey;
-import junit.framework.Assert;
-import junit.framework.TestCase;
 
 
 public class MapTest extends TestCase {
-	private Map<BPlusElementKey, BPlusLeafElement> map = new HashMap<BPlusElementKey, BPlusLeafElement>();
+	private Map<String, BPlusLeafElement> map = new HashMap<String, BPlusLeafElement>();
 	
 	
 	private void _test_insert( String str, Integer value) {
-		BPlusElementKey key = new BPlusElementKey(str);
-		BPlusElementKey key_ = new BPlusElementKey(str);
-		map.put(key, new BPlusLeafElement(key,2) );
-		Assert.assertEquals( value, map.get(key_).getValue() );
+		String key = new String( str );
+		String key_ = new String( str );
+		map.put(key, new BPlusLeafElement(new BPlusElementKey(key),2) );
+		assertEquals( value, map.get(key_).getValue() );
 	}
 	
 	public void test() {
