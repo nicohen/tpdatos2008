18980021d6c126421b4e6e6ede4cd692afadc5e7
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-21 15:03:43 +0000

    quitado getSize del BPlusElement

--- trunk/tpdatos/src/bplus/elements/BPlusAbstractElement.java
+++ trunk/tpdatos/src/bplus/elements/BPlusAbstractElement.java
@@ -5,11 +5,9 @@ import bplus.keys.BPlusElementKey;
 public abstract class BPlusAbstractElement implements BPlusElement {
 	
 	protected BPlusElementKey elementKey;
-	protected Integer size;
 	
 	public BPlusAbstractElement(BPlusElementKey key) {
 		this.elementKey=key;
-		this.size=key.getSize();
 	}
 	
 	public BPlusElementKey getKey() {
@@ -18,10 +16,6 @@ public abstract class BPlusAbstractElement implements BPlusElement {
 	
 	public void setKey(BPlusElementKey elementKey) {
 		this.elementKey = elementKey;
-	}
-	
-	public Integer getSize() {
-		return size;
 	}
 
 	public int compareTo(BPlusElement arg0) {
