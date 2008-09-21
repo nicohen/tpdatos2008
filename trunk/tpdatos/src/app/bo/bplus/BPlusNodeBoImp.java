8d68e098fb73c47321b49396e1948ee8dcdc02f3
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-21 15:26:08 +0000

    Agregada implementacion BPlusNodeBoImp.java (metodos vacios) y BPlusNodeBoTest

---
+++ trunk/tpdatos/src/app/bo/bplus/BPlusNodeBoImp.java
@@ -0,0 +1,37 @@
+package app.bo.bplus;
+
+import bplus.elements.BPlusLeafElement;
+import bplus.exceptions.KeyNotFoundException;
+import bplus.keys.BPlusElementKey;
+import api.bo.BPlusTree.BPlusNodeBo;
+
+public class BPlusNodeBoImp implements BPlusNodeBo {
+
+	@Override
+	public BPlusLeafElement getElement(BPlusElementKey elementKey)
+			throws KeyNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertElement(BPlusLeafElement element) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeElement(BPlusElementKey elementKey)
+			throws KeyNotFoundException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateElement(BPlusLeafElement element)
+			throws KeyNotFoundException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ trunk/tpdatos/src/app/bo/bplus/BPlusNodeBoTest.java
@@ -0,0 +1,51 @@
+package app.bo.bplus;
+
+import java.util.HashMap;
+
+import bplus.elements.BPlusLeafElement;
+import bplus.exceptions.KeyNotFoundException;
+import bplus.keys.BPlusElementKey;
+import api.bo.BPlusTree.BPlusNodeBo;
+
+public class BPlusNodeBoTest implements BPlusNodeBo {
+
+	
+	private HashMap<BPlusElementKey, BPlusLeafElement> map;
+	
+	@Override
+	public BPlusLeafElement getElement(BPlusElementKey elementKey)
+			throws KeyNotFoundException {
+		// TODO Auto-generated method stub
+		BPlusLeafElement element = map.get(elementKey);
+		if ( element == null) {
+			throw new KeyNotFoundException();
+		} else {
+			return element;
+		}
+	}
+
+	@Override
+	public void insertElement(BPlusLeafElement element) {
+		// TODO Auto-generated method stub
+		map.put(element.getKey(), element);
+
+	}
+
+	@Override
+	public void removeElement(BPlusElementKey elementKey)
+			throws KeyNotFoundException {
+		// TODO Auto-generated method stub
+		if (map.get(elementKey) == null) throw new KeyNotFoundException();
+		map.remove(elementKey);
+
+	}
+
+	@Override
+	public void updateElement(BPlusLeafElement element)
+			throws KeyNotFoundException {
+		// TODO Auto-generated method stub
+		if (map.get(element.getKey()) == null) throw new KeyNotFoundException();
+		map.put(element.getKey(), element);
+	}
+
+}
