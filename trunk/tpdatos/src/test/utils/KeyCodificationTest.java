59ba255f603335a0a69d3c580a3bd270784886d2
berta1108 <berta1108@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-21 17:47:50 +0000

    Sale unario como pinia

--- trunk/tpdatos/src/test/utils/KeyCodificationTest.java
+++ trunk/tpdatos/src/test/utils/KeyCodificationTest.java
@@ -1,5 +1,7 @@
 package test.utils;
 
+import java.util.BitSet;
+
 import junit.framework.TestCase;
 import utils.KeyCodificationUtils;
 
@@ -10,43 +12,18 @@ public class KeyCodificationTest extends TestCase {
 //		assertEquals(48,(char)gamma1[0].intValue());	
 //	}
 	
-	public void testGammaEncode() {
-		//0
-		Byte[] gamma1 = KeyCodificationUtils.gammaEncode(1);
-		assertEquals(48,(char)gamma1[0].intValue());	
-
-		//100
-		Byte[] gamma2 = KeyCodificationUtils.gammaEncode(2);
-		assertEquals(49,gamma2[0].intValue());	
-		assertEquals(48,gamma2[1].intValue());
-		assertEquals(48,gamma2[2].intValue());
-
-		//101
-		Byte[] gamma3 = KeyCodificationUtils.gammaEncode(3);
-		assertEquals(49,gamma3[0].intValue());	
-		assertEquals(48,gamma3[1].intValue());
-		assertEquals(49,gamma3[2].intValue());
-
-		//11000
-		Byte[] gamma4 = KeyCodificationUtils.gammaEncode(4);
-		assertEquals(49,gamma4[0].intValue());	
-		assertEquals(49,gamma4[1].intValue());
-		assertEquals(48,gamma4[2].intValue());
-		assertEquals(48,gamma4[3].intValue());
-		assertEquals(48,gamma4[4].intValue());
-
-		//FIXME: Esta prueba falla, deberia devolver 11111010000
-		Byte[] gamma48 = KeyCodificationUtils.gammaEncode(48);
-		assertEquals(49,gamma48[0].intValue());	
-		assertEquals(49,gamma48[1].intValue());
-		assertEquals(49,gamma48[2].intValue());
-		assertEquals(49,gamma48[3].intValue());
-		assertEquals(49,gamma48[4].intValue());
-		assertEquals(48,gamma48[5].intValue());
-		assertEquals(49,gamma48[6].intValue());
-		assertEquals(48,gamma48[7].intValue());
-		assertEquals(48,gamma48[8].intValue());
-		assertEquals(48,gamma48[9].intValue());
-		assertEquals(48,gamma48[10].intValue());
-	}
+	public void testUnaryEncode() {
+		Integer num1=8,num2=5;
+		//11111110
+		byte[] gamma1 = KeyCodificationUtils.unarioEncode(num1);
+		Integer size1=KeyCodificationUtils.unarioDecode(gamma1);
+		size1+=1;
+		assertEquals(size1,num1);
+		//11110
+		byte[] gamma2 = KeyCodificationUtils.unarioEncode(num2);
+		BitSet set=new BitSet();
+		Integer size2=KeyCodificationUtils.unarioDecode(gamma2);
+		size2+=1;
+		assertEquals(size2,num2);
+		}
 }
--- trunk/tpdatos/src/utils/KeyCodificationUtils.java
+++ trunk/tpdatos/src/utils/KeyCodificationUtils.java
@@ -1,16 +1,19 @@
 package utils;
 
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.BitSet;
+
 
 public class KeyCodificationUtils {
 
-	public static Byte[] unarioEncode(Integer key) {
+	public static byte[] unarioEncode(Integer key) {
 		
 		BitSet cjto=new BitSet(key);
 		
-		for(int i=0;i<cjto.length();i++){
+		for(int i=0;i<key;i++){
 			cjto.set(i,true);
-			if (i==cjto.length())
+			if (i==key-1)
 			cjto.set(i,false);
 		}
 		
@@ -19,44 +22,45 @@ public class KeyCodificationUtils {
 	}
 	
 	
-	public static Integer unarioDecode(Byte[] key) {
+	public static Integer unarioDecode(byte[] key) {
 		return fromByteArray(key).length();
 	}
-	
+	/*
 	public static Byte[] gammaEncode(Integer key) { 
 		Byte[] gamma = null;
+		BitSet cjto=new BitSet();
+		int i,j=0;
 		if (key==1) {
-			gamma = new Byte[1];
-			gamma[0]=48;
-			return gamma;
+			cjto=new BitSet(key);
+			cjto.set(0,false);
+			return toByteArray(cjto);
 		}
+		
 		String gammaStr = "";
-		String logaritmoEnBase2DeN = String.valueOf((int)Math.floor(Math.log(key)/Math.log(2)));
-		for (int i=0;i<Integer.parseInt(logaritmoEnBase2DeN);i++) {
-			gammaStr += 1;	
+		Integer logaritmoEnBase2DeN = Integer.valueOf((int)Math.floor(Math.log(key)/Math.log(2)));
+		for (i=0;i<logaritmoEnBase2DeN;i++) {
+			if (i==logaritmoEnBase2DeN)
+			cjto.set(i,false);
+			else
+			cjto.set(i,true);
 		}
-		gammaStr += 0;
-		String ultimos = String.valueOf((key-((int)Math.pow(2,Integer.parseInt(logaritmoEnBase2DeN)))));
-		for (int i=0;i<Integer.parseInt(logaritmoEnBase2DeN)-ultimos.length();i++) {
-			gammaStr+=0;
+		Integer parteBinaria = Integer.valueOf((key-((int)Math.pow(2,logaritmoEnBase2DeN))));
+		String binario= Integer.toBinaryString(parteBinaria);
+		for (;i<logaritmoEnBase2DeN+binario.length();++i) {
+			cjto.set(i,Boolean.parseBoolean(binario.substring(j, j+1)));
+			j++;
 		}
-		for (int i=0;i<ultimos.length();i++) {
-			gammaStr+=ultimos.charAt(i);
-		}
-		gamma = new Byte[gammaStr.length()];
-		for (int i=0;i<gammaStr.length();i++) {
-			gamma[i]=(byte)gammaStr.charAt(i);
-		}
-		return gamma;
+		
+		return toByteArray(cjto);
 	}
-	
+	*/
 	public static Integer gammaDecode(Byte[] key) { 
 		//TODO - Implementar gammaDecode
 		return null; 
 	}
 	
-	public static Byte[] toByteArray(BitSet bits) {
-        Byte[] bytes = new Byte[bits.length()/8+1];
+	public static byte[] toByteArray(BitSet bits) {
+        byte[] bytes = new byte[bits.length()/8+1];
         for (int i=0; i<bits.length(); i++) {
             if (bits.get(i)) {
                 bytes[bytes.length-i/8-1] |= 1<<(i%8);
@@ -64,8 +68,20 @@ public class KeyCodificationUtils {
         }
         return bytes;
     }
-	
-	public static BitSet fromByteArray(Byte[] bytes) {
+	/*
+	public static byte[] toByteArray(BitSet bitSet) {
+	     ByteArrayOutputStream baos = new ByteArrayOutputStream(bitSet.size());
+	     try {
+	       ObjectOutputStream oos = new ObjectOutputStream(baos);
+	       oos.writeObject(bitSet);
+	     }
+	     catch (Exception ex) {
+	       ex.printStackTrace();
+	     }
+	     return baos.toByteArray();
+	  }
+	*/
+	public static BitSet fromByteArray(byte[] bytes) {
         BitSet bits = new BitSet();
         for (int i=0; i<bytes.length*8; i++) {
             if ((bytes[bytes.length-i/8-1]&(1<<(i%8))) > 0) {
