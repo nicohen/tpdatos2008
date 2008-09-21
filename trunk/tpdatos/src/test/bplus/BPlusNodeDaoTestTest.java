cab51e3a3418542d1b1b399ace6b6c7e855669f7
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-21 18:25:54 +0000

    El BPlusNodeDaoTest pasa las prubas (junit del dao)

---
+++ trunk/tpdatos/src/test/bplus/BPlusNodeDaoTestTest.java
@@ -0,0 +1,43 @@
+package test.bplus;
+
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.junit.Before;
+
+import exceptions.DataAccessException;
+
+import bplus.keys.BPlusNodeKey;
+import bplus.nodes.BPlusLeafNode;
+import bplus.nodes.BPlusNode;
+
+import app.dao.bplus.BPlusNodeDaoTest;
+
+public class BPlusNodeDaoTestTest extends TestCase {
+	
+	
+	private BPlusNodeDaoTest dao = new BPlusNodeDaoTest();
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	public Integer _test_insert(BPlusNode node) throws DataAccessException{
+		BPlusNodeKey key = dao.insertNode(node);
+
+		BPlusNode nodoInsertado = dao.getNode(key);
+		
+		assert( nodoInsertado.equals(node) );
+		
+		return key.getValue();
+		
+	}
+	
+	public void test() throws DataAccessException {
+		
+		BPlusNode node = new BPlusLeafNode();
+		_test_insert(node);
+	}
+	
+}
