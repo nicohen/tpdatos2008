898481cf05aa5c9afcdce77697da29f854b451e2
berta1108 <berta1108@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-22 16:41:01 +0000

    Gamma encode/decode, unario encode/decode, testeados 100% OK. Ah, alguien subio algo que no compila, guarda.

--- trunk/tpdatos/src/test/utils/KeyCodificationTest.java
+++ trunk/tpdatos/src/test/utils/KeyCodificationTest.java
@@ -17,22 +17,18 @@ public class KeyCodificationTest extends TestCase {
 		//11111110
 		byte[] gamma1 = KeyCodificationUtils.unarioEncode(num1);
 		Integer size1=KeyCodificationUtils.unarioDecode(gamma1);
-		size1+=1;
 		assertEquals(size1,num1);
 		//11110
 		byte[] gamma2 = KeyCodificationUtils.unarioEncode(num2);
 		BitSet set=new BitSet();
 		Integer size2=KeyCodificationUtils.unarioDecode(gamma2);
-		size2+=1;
 		assertEquals(size2,num2);
 		}
 	public void testGammaEncode(){
-		Integer num1=9,num2=5;
-		//11111110
+		Integer num1=9111,num2=5;
 		byte[] gamma1 = KeyCodificationUtils.gammaEncode(num1);
 		Integer size1=KeyCodificationUtils.gammaDecode(gamma1);
-		//size1+=1;
-		//assertEquals(size1,num1);
+		assertEquals(size1,num1);
 		}
 }
 
