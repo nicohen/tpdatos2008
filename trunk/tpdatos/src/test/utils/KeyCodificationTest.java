97a04d2f8cbc5549a1c4e281e2525b1c52e8271a
berta1108 <berta1108@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-22 16:42:20 +0000

    unos warnings de cosas al dope

--- trunk/tpdatos/src/test/utils/KeyCodificationTest.java
+++ trunk/tpdatos/src/test/utils/KeyCodificationTest.java
@@ -20,12 +20,11 @@ public class KeyCodificationTest extends TestCase {
 		assertEquals(size1,num1);
 		//11110
 		byte[] gamma2 = KeyCodificationUtils.unarioEncode(num2);
-		BitSet set=new BitSet();
 		Integer size2=KeyCodificationUtils.unarioDecode(gamma2);
 		assertEquals(size2,num2);
 		}
 	public void testGammaEncode(){
-		Integer num1=9111,num2=5;
+		Integer num1=123456789;
 		byte[] gamma1 = KeyCodificationUtils.gammaEncode(num1);
 		Integer size1=KeyCodificationUtils.gammaDecode(gamma1);
 		assertEquals(size1,num1);
