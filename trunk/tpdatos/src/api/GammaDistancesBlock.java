1058751cb641dd5d7880128d2964037c80a83334
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-28 14:20:19 +0000

    agregado el bloque que codifica con gamma (sin implementacion de sus metodos) y el persistor del linkedblock

---
+++ trunk/tpdatos/src/api/GammaDistancesBlock.java
@@ -0,0 +1,33 @@
+package api;
+
+import exceptions.PersistanceException;
+import api.po.persistors.DistancesBlock;
+
+public class GammaDistancesBlock implements DistancesBlock {
+
+	
+	
+	// el modificador de acceso del constructor es package
+	// para que solo las clases de este paquete puedan
+	// instanciarla
+	
+	GammaDistancesBlock( int maxsize ) {
+		
+	}
+	
+	public int[] decodeDistances() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void encodeDistance(int distance) throws PersistanceException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public byte[] toByteArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
