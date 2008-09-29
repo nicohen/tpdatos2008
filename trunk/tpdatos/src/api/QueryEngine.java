917da046c694cad9fc84d8ce7d91d60ecbd8bb02
berta1108 <berta1108@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-29 21:37:42 +0000

    QueryEngine, clase motor de consultas. Falta ver o como se implemento el diccionario de documentos ya que se llama esta clase (esta clase usa indexImp para buscar y mapea con el diccionario)

---
+++ trunk/tpdatos/src/api/QueryEngine.java
@@ -0,0 +1,32 @@
+package api;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import exceptions.BusinessException;
+import app.bo.IndexImp;
+
+public class QueryEngine {
+	private IndexImp indice;
+	
+	public QueryEngine(IndexImp index){
+		
+		indice=index;
+		
+	}
+	
+	public Iterator<String> ExecuteQuery(String consulta) throws BusinessException{
+		ArrayList<String> listaRet=new ArrayList<String>();
+		ArrayList<Integer> listaAux=new ArrayList<Integer>();
+		Iterator<Integer> itAux;
+		
+		itAux=indice.getDocuments(consulta);
+		
+		while (itAux.hasNext()){
+			//listaRet.add(Dictionary.GetStringFromId(itAux.next()));
+		}
+		
+		return listaRet.iterator();
+	}
+	
+}
