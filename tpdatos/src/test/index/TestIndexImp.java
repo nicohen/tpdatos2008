80fdd9b8dcfb5ad58d0c66656f21efdbc9afddde
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-29 00:17:48 +0000

    agregado testcase para prueba de Index, de MockIndex y de IndexImp

---
+++ tpdatos/src/test/index/TestIndexImp.java
@@ -0,0 +1,18 @@
+package test.index;
+
+import exceptions.BusinessException;
+import api.bo.Index;
+import app.bo.IndexImp;
+
+public class TestIndexImp extends TestIndex {
+
+	public TestIndexImp() throws BusinessException {
+		super();
+	}
+
+	@Override
+	protected Index createTestObject() throws BusinessException {
+		return new IndexImp() ;
+	}
+
+}
