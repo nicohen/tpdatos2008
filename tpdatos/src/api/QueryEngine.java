de27078de880052af8a9ec1e1d7b276f9880bb61
berta1108 <berta1108@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-09-30 18:04:13 +0000

    interfaz con diccionario docs REAL, no mock

--- tpdatos/src/api/QueryEngine.java
+++ tpdatos/src/api/QueryEngine.java
@@ -3,30 +3,33 @@ package api;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import dto.DocumentDto;
+
+import api.dao.documents.DocumentsDictionary;
 import app.bo.IndexImp;
 import exceptions.BusinessException;
 
 public class QueryEngine {
 	private IndexImp indice;
-	
-	public QueryEngine(IndexImp index){
-		
+	private DocumentsDictionary dicc;
+	public QueryEngine(IndexImp index,DocumentsDictionary dictionary){
+		dicc=dictionary;
 		indice=index;
 		
 	}
 	
-	public Iterator<Integer> ExecuteQuery(String consulta) throws BusinessException{
-		ArrayList<String> listaRet=new ArrayList<String>();
+	public Iterator<DocumentDto> ExecuteQuery(String consulta) throws BusinessException{
+		ArrayList<DocumentDto> listaRet=new ArrayList<DocumentDto>();
 		ArrayList<Integer> listaAux=new ArrayList<Integer>();
 		Iterator<Integer> itAux;
 		
 		itAux=indice.getDocuments(consulta);
 		
-		//while (itAux.hasNext()){
-			//listaRet.add(Dictionary.GetStringFromId(itAux.next()));
-		//}
+		while (itAux.hasNext()){
+			listaRet.add(dicc.getDocument(itAux.next()));
+		}
 		
-		return itAux;
+		return listaRet.iterator();
 	}
 	
 }
