c381e1d88b64cb09d34144e0953a01c1207f84f3
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-01 00:38:02 +0000

    terminado el comparador de elementos del nodo

--- tpdatos/src/test/bplus/BPlusNodeTestUtils.java
+++ tpdatos/src/test/bplus/BPlusNodeTestUtils.java
@@ -8,28 +8,19 @@ import junit.framework.Assert;
 import bplus.elements.BPlusElement;
 
 public class BPlusNodeTestUtils {
+
 	
 	public static void assertElementsEquals(
 			List<BPlusElement> elements1,
 			List<BPlusElement> elements2) {
-		
-			HashSet<BPlusElement> set1 = new HashSet<BPlusElement>();
-			HashSet<BPlusElement> set2 = new HashSet<BPlusElement>();
+
 		
 			for (BPlusElement e : elements1) {
-				set1.add(e);
+				Assert.assertTrue(elements2.contains(e));
 			}
 
 			for (BPlusElement e : elements2) {
-				set2.add(e);
-			}
-
-			for (BPlusElement e : elements1) {
-				Assert.assertTrue( set2.contains(e) );
-			}
-
-			for (BPlusElement e : elements2) {
-				Assert.assertTrue( set1.contains(e) );
+				Assert.assertTrue(elements1.contains(e));
 			}
 
 	}
