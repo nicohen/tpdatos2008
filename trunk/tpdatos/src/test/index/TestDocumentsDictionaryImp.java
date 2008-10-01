f26cc3e141fa1b4ba55777f007da17ff27f3db37
berta1108 <berta1108@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-01 01:02:10 +0000

    prueba del diccionariio

---
+++ trunk/tpdatos/src/test/index/TestDocumentsDictionaryImp.java
@@ -0,0 +1,12 @@
+package test.index;
+
+import api.dao.documents.DocumentsDictionary;
+
+public class TestDocumentsDictionaryImp extends TestDocumentsDictionary {
+
+	public TestDocumentsDictionaryImp(DocumentsDictionary dict) {
+		super(dict);
+		// TODO Auto-generated constructor stub
+	}
+
+}
