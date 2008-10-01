3a25373982c34a81a1aa2288b4cbeec1b99b54f5
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-01 03:24:23 +0000

    implementado metodo get del StreamFile

--- trunk/tpdatos/src/app/po/files/StreamFile.java
+++ trunk/tpdatos/src/app/po/files/StreamFile.java
@@ -1,23 +1,26 @@
 package app.po.files;
 
+import java.io.ByteArrayOutputStream;
+import java.util.LinkedList;
+
 import api.po.persistors.Persistor;
 import exceptions.DataAccessException;
 import exceptions.PersistanceException;
 
 
-public class StreamFile<E> {
+public class StreamFile {
 	
-	private RelativeFile<E> relativo;
-	private E separator;
+	private RelativeFile<Byte> relativo;
+	private Byte separator;
 	
-	public StreamFile( String path, Persistor<E> persistor , E separator ) throws DataAccessException {
+	public StreamFile( String path, Persistor<Byte> persistor , Byte separator ) throws DataAccessException {
 		this.separator = separator;
-		relativo = new RelativeFile<E>(path,persistor);
+		relativo = new RelativeFile<Byte>(path,persistor);
 	}
 	
 	// devuelve la direccion a donde agrego la secuencia
 	// de registros, persistidos uno tras otro
-	public int add( E[] vect )  throws DataAccessException {
+	public int add( byte[] vect )  throws DataAccessException {
 		boolean primero = true;
 		int direccion = 0;
 
@@ -26,7 +29,7 @@ public class StreamFile<E> {
 			if (vect.length == 0) {
 				throw new DataAccessException();
 			}
-			for ( E e : vect ) {
+			for ( Byte e : vect ) {
 				if (primero ) {
 					direccion = relativo.add(e);
 					primero = false;
@@ -46,9 +49,30 @@ public class StreamFile<E> {
 
 	}
 	
-	public E[] get(int direccion) throws DataAccessException {
+	public byte[] get(int direccion) throws DataAccessException {
 
-		return null;
+		// esta longitud maxima es un tema algoritmico
+		// NO de organizacion, es para saber cuantas
+		// posiciones alocar en el array
+		LinkedList<Byte> list = new LinkedList<Byte>();
+		
+		while (true) {
+			Byte aux = relativo.get(direccion);
+			if (aux.equals(this.separator)) break;
+			
+			list.add(aux);
+			
+			direccion++;
+		}
+		
+		byte[] retorno = new byte[list.size()];
+		
+		int i = 0;
+		for (Byte aux : list) {
+			retorno[i] = aux.byteValue();
+			i++;
+		}
+		return retorno;
 	}
 
 
