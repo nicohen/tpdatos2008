7c39eff543c845b0caf153a4c46d2c5bbeb95ef0
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-02 20:39:45 +0000

    organizacion de de los testcases del imp, metodo de prueba de queryengine implementado en queryengine

--- tpdatos/src/test/index/TestIndexImp.java
+++ tpdatos/src/test/index/TestIndexImp.java
@@ -1,14 +1,7 @@
 package test.index;
 
-import java.util.Iterator;
-
-import api.QueryEngine;
-import api.dao.documents.DocumentsDictionary;
 import app.bo.IndexImp;
-import app.dao.documents.DocumentsDictionaryImp;
-import dto.DocumentDto;
 import exceptions.BusinessException;
-import exceptions.DataAccessException;
 
 public class TestIndexImp extends TestIndex {
 
@@ -21,37 +14,5 @@ public class TestIndexImp extends TestIndex {
 		return new IndexImp("index.bin", 4096) ;
 	}
 
-	
-	public void testInsertRetrieve() throws BusinessException, DataAccessException{
-		IndexImp index = this.createTestObject();
-		DocumentsDictionary dicc=new DocumentsDictionaryImp();
-		DocumentDto doc=new DocumentDto(0,"Documento perro");
-		DocumentDto doc2=new DocumentDto(1,"Documento gato");
-		dicc.insertDocument(doc);
-		dicc.insertDocument(doc2);
-		QueryEngine engine=new QueryEngine(index,dicc);
-		Iterator<DocumentDto> it;
-		index.insertWord("perro", 0);
-		/*index.insertWord("perro", 2);
-		index.insertWord("perro", 3);
-		index.insertWord("perro", 4);
-		index.insertWord("perro", 5);*/
-		index.insertWord("gato", 1);
-		/*index.insertWord("gato", 7);
-		index.insertWord("gato", 8);
-		index.insertWord("gato", 9);
-		index.insertWord("gato", 10);*/
-		
-		int i=6;
-		
-		for(DocumentDto document : engine.executeQuery("gato")){
-			//Assert.assertTrue(document.getDocumentId()==i);
-			if (i==10)
-			i=6;
-			else
-			i++;
-		}
-		
-	}
 
 }
