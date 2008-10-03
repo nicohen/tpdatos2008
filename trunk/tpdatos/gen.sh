ab3e7c474e0a2ce9ce8d5dd2a82c7f9b0e817f4e
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-03 17:17:45 +0000

    cambiado makefile para generar los .class al directorio bin

--- trunk/tpdatos/gen.sh
+++ trunk/tpdatos/gen.sh
@@ -21,13 +21,19 @@ echo >> makefile
 echo "CLASSES = "$( find -name '*.java' ) >> makefile
 echo  >> makefile
 echo "default: classes"  >> makefile
-echo 'classes:   $(CLASSES:.java=.class)'  >> makefile
+#echo 'classes:   $(CLASSES:.java=.class)'  >> makefile
+echo "classes: "$(find -name '*.java'  | sed -e 's/\.\/src/\.\/bin/' -e 's/\.java$/\.class/') >> makefile
 echo 'clean:
 	   rm $(CLASSES:.java=.class)'  >> makefile
 
 echo >> makefile
 
-find -name '*.java' | sed -e 's/\.java//' | awk '{ print  $1 ".class: " $1 ".java\n\t\t$(JC) $(JFLAGS) " $1 ".java" }' >> makefile
+cd src
+
+find -name '*.java'| sed 's/\.\///' | sed -e 's/\.java//' | awk '{ print  "bin/" $1 ".class: src/" $1 ".java\n\t\t$(JC) $(JFLAGS) src/" $1 ".java && mv src/" $1 ".class bin/" $1 ".class " }' >> ../makefile
+
+cd ..
+
 
 echo >> makefile
 echo >> makefile
