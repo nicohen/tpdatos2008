0cda0b8e3956e517c66b7fd1563059eaec2f76e5
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-04 15:08:09 +0000

    "descableado" el +1 de DocumentIndexer, en lo proximo se podria agregar en el linkedblocks, donde es necesario

--- trunk/tpdatos/src/DocumentsIndexer.java
+++ trunk/tpdatos/src/DocumentsIndexer.java
@@ -60,7 +60,7 @@ public class DocumentsIndexer {
 					
 					log.info("Indexando documento ["+document.getFileName()+"]");
 					//Le seteo un nuevo documentId al documento a indexar
-					document.setDocumentId(indexedDocuments.getNewDocumentId()+1 );
+					document.setDocumentId(indexedDocuments.getNewDocumentId() );
 					
 					//Elimino tags y caracteres que no correspondan
 					File inputFile = new File(Constants.FOLDER_DOCUMENTS + File.separator + document.getFileName());
