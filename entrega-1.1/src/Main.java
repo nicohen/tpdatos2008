6c2b11b32c47f26bd186a5138c12ece07c9b0efb
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-09 02:21:54 +0000

    agregado main que invoca a los otros tres segun la opcion

---
+++ entrega-1.1/src/Main.java
@@ -0,0 +1,28 @@
+
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		String option = "--indexer";
+		if ( args.length < 0) {
+			option = args[0];
+		}
+
+		if (option.equals("--indexer" ) ) {
+			DocumentsIndexer.main(args);
+		} else if (option.equals("--tree-dumper")) {
+			TreeDumper.main(args);
+		} else if ( option.equals("--search")) {
+			SearchEngine.main(args);
+		} else {
+			System.out.println("Opcion invalida");
+		}
+		
+	}
+}
