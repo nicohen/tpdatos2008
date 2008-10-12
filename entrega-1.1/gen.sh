8f0579db4001cd1f305cb4660563de0bc23d7865
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-12 04:11:28 +0000

    generado makefile a partir de la ultima version de gen.sh

--- entrega-1.1/gen.sh
+++ entrega-1.1/gen.sh
@@ -18,20 +18,33 @@ echo 'JC = javac' >> makefile
 echo >> makefile
 echo >> makefile
 
+CLASSES=$(find -name '*.java'  | sed -e 's/\.java$/\.class/')
+CLASSESSRC=$(find -name '*.java'  | sed -e 's/src\///' -e 's/\.java$/\.class/')
+CLASSESBIN=$(find -name '*.java'  | sed -e 's/\.\/src\///' -e 's/\.java$/\.class/')
+
+
 echo "CLASSES = "$( find -name '*.java' ) >> makefile
 echo  >> makefile
 echo "default: classes"  >> makefile
-#echo 'classes:   $(CLASSES:.java=.class)'  >> makefile
-echo "classes: "$(find -name '*.java'  | sed -e 's/\.\/src/\.\/bin/' -e 's/\.java$/\.class/') >> makefile
+echo "classes: "$CLASSES >> makefile
 echo 'clean:
 	   rm $(CLASSES:.java=.class)'  >> makefile
+echo 'jar:
+	cd src && jar cvfm ../bombita.jar ../manifesto.txt *'>> makefile
+
+echo 'dist:
+	tar -c '$( find -name "*.java" )' words/stemming.txt words/stopwords.txt makefile '$(find lib/ -name '*.jar')' > bombita-src.tar 
+	gzip --force bombita-src.tar 
+	@echo "*************************************************"
+	@echo "* 	bombita-src.tar.gz generated	       *"
+	@echo "*************************************************" ' >> makefile 
 
 echo >> makefile
 
 cd src
 
 #find -name '*.java'| sed 's/\.\///' | sed -e 's/\.java//' | awk '{ print  "bin/" $1 ".class: src/" $1 ".java\n\t\t$(JC) $(JFLAGS) src/" $1 ".java && mv src/" $1 ".class bin/" $1 ".class " }' >> ../makefile
-find -name '*.java'| sed 's/\.\///' | sed -e 's/\.java//' | awk '{ print  "bin/" $1 ".class: src/" $1 ".java\n\t\t$(JC) $(JFLAGS) src/" $1 ".java" }' >> ../makefile
+find -name '*.java'| sed 's/\.\///' | sed -e 's/\.java//' | awk '{ print  "src/" $1 ".class: src/" $1 ".java\n\t\t$(JC) $(JFLAGS) src/" $1 ".java" }' >> ../makefile
 
 cd ..
 
