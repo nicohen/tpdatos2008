906e6942d8c7883bd3b2999d30e4a96d62f952fd
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-12 13:03:49 +0000

    implementado uso de constantes en DefaultQueryEngine

--- trunk/tpdatos/src/api/DefaultQueryEngine.java
+++ trunk/tpdatos/src/api/DefaultQueryEngine.java
@@ -2,6 +2,7 @@ package api;
 
 
 
+import utils.Constants;
 import app.bo.IndexImp;
 import app.bo.MockDocumentsDictionary;
 import app.dao.documents.DocumentsDictionaryImp;
@@ -12,7 +13,7 @@ public class DefaultQueryEngine extends QueryEngine {
 
 	public DefaultQueryEngine() throws BusinessException, DataAccessException {
 		super(	
-				new IndexImp("index.bin",4096)
+				new IndexImp(Constants.INDEX_FILE_PATH, Constants.INDEX_FILE_SIZE)
 				,
 				new DocumentsDictionaryImp()
 				);
