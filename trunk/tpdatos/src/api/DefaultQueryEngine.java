4fa814f43edd0b60f75352e3114a9f9a729d6ee6
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-12 12:58:08 +0000

    definida utilizacion de DocumentDictionaryImp en DefaultQueryEngine

--- trunk/tpdatos/src/api/DefaultQueryEngine.java
+++ trunk/tpdatos/src/api/DefaultQueryEngine.java
@@ -4,15 +4,17 @@ package api;
 
 import app.bo.IndexImp;
 import app.bo.MockDocumentsDictionary;
+import app.dao.documents.DocumentsDictionaryImp;
 import exceptions.BusinessException;
+import exceptions.DataAccessException;
 
 public class DefaultQueryEngine extends QueryEngine {
 
-	public DefaultQueryEngine() throws BusinessException {
+	public DefaultQueryEngine() throws BusinessException, DataAccessException {
 		super(	
 				new IndexImp("index.bin",4096)
 				,
-				new MockDocumentsDictionary()
+				new DocumentsDictionaryImp()
 				);
 	}
 
