a7fbd8de198f89fc73f92af6764be81fc639d252
berta1108 <berta1108@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-13 22:31:01 +0000

    habia perros en el codigo

--- tpdatos/src/test/index/TestLinkedBlocksManager.java
+++ tpdatos/src/test/index/TestLinkedBlocksManager.java
@@ -11,11 +11,11 @@ import linkedblocks.utils.KeyCodificationUtils;
 
 public class TestLinkedBlocksManager extends TestCase {
 	public void test() throws DataAccessException {
-		LinkedBlockByteArrayPersistor persistor=new LinkedBlockByteArrayPersistor(64);
+		LinkedBlockByteArrayPersistor persistor=new LinkedBlockByteArrayPersistor(4096);
 		Iterator<byte[]> it;
 		int j=0;
 		VariableLinkedBlocksManager manager=new VariableLinkedBlocksManager("index1.bin", persistor);
-		for(int i=0;i<400;i++)
+		for(int i=0;i<40000;i++)
 		manager.add(KeyCodificationUtils.gammaEncode(i), 0);
 		//for(int i=0;i<400;i++)
 		it=manager.get(0);
