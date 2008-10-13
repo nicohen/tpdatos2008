e5cf7b7dc7443707a26337643a230349b7f87fba
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-13 14:25:23 +0000

    correcciones al persistor de GammaDistancesBlock, para poder peristir los bloques de tamaño fijo

--- trunk/tpdatos/src/test/index/TestIntegerLinkedBlocksManager.java
+++ trunk/tpdatos/src/test/index/TestIntegerLinkedBlocksManager.java
@@ -14,7 +14,7 @@ public class TestIntegerLinkedBlocksManager extends
 		TestLinkedBlocksManager<Integer> {
 
 	public TestIntegerLinkedBlocksManager() throws DataAccessException {
-		super(new IntegerPersistor() );
+		super(new IntegerPersistor(), "linkedblock-integer-test.bin" );
 	}
 	
 	
@@ -23,17 +23,17 @@ public class TestIntegerLinkedBlocksManager extends
 
 		
 		linked.add(0);
-		linked.add(1);
-		linked.add(2);
-		int ultimobloque = linked.add(3);
-		linked.add(9,ultimobloque);
-		linked.add(22,ultimobloque);
+		linked.add(0x11);
+		linked.add(0x22);
+		int ultimobloque = linked.add(0x33);
+		linked.add(0x19,ultimobloque);
+		linked.add(0x22,ultimobloque);
 		
 		Iterator<Integer> it = linked.get(ultimobloque);
 		
-		Assert.assertEquals ( 3, it.next().intValue() );
-		Assert.assertEquals ( 9, it.next().intValue() );
-		Assert.assertEquals ( 22, it.next().intValue() );
+		Assert.assertEquals ( 0x33, it.next().intValue() );
+		Assert.assertEquals ( 0x19, it.next().intValue() );
+		Assert.assertEquals ( 0x22, it.next().intValue() );
 	}
 
 }
