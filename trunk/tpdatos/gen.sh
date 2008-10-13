eb3344fc8e0dbff5fbf1fe0e41fb39c2a0389bfe
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-13 17:45:17 +0000

    correccion al makefile: incorportado manifesto al dist para que se puede generar el jar

--- trunk/tpdatos/gen.sh
+++ trunk/tpdatos/gen.sh
@@ -33,7 +33,7 @@ echo 'jar:
 	cd src && jar cfm ../bombita.jar ../manifesto.txt *'>> makefile
 
 echo 'dist:
-	tar -c '$( find -name "*.java" )' words/stemming.txt words/stopwords.txt makefile '$(find lib/ -name '*.jar')' > bombita-src.tar 
+	tar -c '$( find -name "*.java" )' manifesto.txt words/stemming.txt words/stopwords.txt makefile '$(find lib/ -name '*.jar')' > bombita-src.tar 
 	gzip --force bombita-src.tar 
 	@echo "*************************************************"
 	@echo "* 	bombita-src.tar.gz generated	       *"
