11d2c0321363322c6c4a7f4968f2699e991fbbee
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-10-24 19:09:13 +0000

    Subo distintas funciones de hashing en SignatureUtils

--- trunk/tpdatos/src/signaturefiles/SignatureGenerator.java
+++ trunk/tpdatos/src/signaturefiles/SignatureGenerator.java
@@ -4,11 +4,6 @@ import dto.SignatureFileDto;
 
 public class SignatureGenerator {
 
-	//Devuelve el signature del termino ingresado
-	public SignatureFileDto getSignature(String word) {
-		return null;
-	}
-	
 	//Realiza un Or entre las dos signatures
 	public SignatureFileDto orGate(SignatureFileDto signature1, SignatureFileDto signature2) {
 		return null;
