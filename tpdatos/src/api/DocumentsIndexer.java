d5e28f6871ee5b7114dd47d528d098a7556975b6
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-11-12 00:55:07 +0000

    Agregados de excepciones y modificaciones en mensajes

--- tpdatos/src/api/DocumentsIndexer.java
+++ tpdatos/src/api/DocumentsIndexer.java
@@ -20,7 +20,6 @@ import dto.DocumentDto;
 import dto.StopwordsPipelineDto;
 import dto.WordDto;
 import exceptions.BusinessException;
-import exceptions.DataAccessException;
 import exceptions.PipelineOverflowException;
 
 /**
@@ -40,7 +39,7 @@ public class DocumentsIndexer {
 	private StopwordsProcessor stopwordsProcessor;
 	private StemmingProcessor stemmingProcessor;
 
-	public DocumentsIndexer(IQueryEngine pqueryEngine) throws BusinessException, DataAccessException {
+	public DocumentsIndexer(IQueryEngine pqueryEngine) {
 		//queryEngine = new DefaultQueryEngine();
 		this.queryEngine = pqueryEngine;
 		//Inicializo las stopwords y las ordeno alfabeticamente y por cantidad de palabras ascendente
@@ -51,98 +50,102 @@ public class DocumentsIndexer {
 	}
 	
 	
-	public int indexDocument( DocumentDto document, String logtag ) throws ParserException, BusinessException, PipelineOverflowException {
+	public int indexDocument( DocumentDto document, String logtag ) throws BusinessException {
 		
 		int totalIndexed = 0;
+		File inputFile = null;
 
-		DocumentInsert docInsert = queryEngine.prepareDocumentInsert(document.getFileName() ); 
+		try {
 		
-		log.info("Indexando documento ["+document.getFileName()+"]");
-		//Le seteo un nuevo documentId al documento a indexar
-		
-		// NOTE: da lo mismo qye ud tenga el documento, query engine lo
-		// define internamente dependiendo de cual id nuevo define el diccionario
-		// de documentos
-		
-		//document.setDocumentId(indexedDocuments.getNewDocumentId() );
-		
-		//Elimino tags y caracteres que no correspondan
-		File inputFile = new File(Constants.FOLDER_DOCUMENTS + File.separator + document.getFileName());
-		String documentText = DigesterUtils.getFormattedHtmlFile(inputFile);
-		
-		//Inicializo el pipeline para encolar terminos 
-		StopwordsPipelineDto stopwordsPipeline = new StopwordsPipelineDto();
-		
-		//Me inicializo un tokenizer para recorrer los terminos del documento
-		StringTokenizer strTok = new StringTokenizer(documentText, " \t\n\r\f");
-
-		//Hago un preload de terminos en el pipeline
-		while(strTok.hasMoreTokens() && stopwordsPipeline.getSize()<(stopwordsPipeline.getMaxSize()-1)) {
-			String token = (strTok.nextToken()).trim();
-			if (!"".equals(token) && token!=null) {
-				stopwordsPipeline.addWord(token);
-			}
-		}
-		
-		while(strTok.hasMoreTokens() || !stopwordsPipeline.isEmpty()) {
+			DocumentInsert docInsert = queryEngine.prepareDocumentInsert(document.getFileName() ); 
 			
-			boolean hasMoreTokens = false;
-			String token = null;
-			if(strTok.hasMoreTokens()) {
-				 token = (strTok.nextToken()).trim();
-				 hasMoreTokens = true;
-			}
-
-			//Verifico que exista el termino
-			if ((!"".equals(token) && token!=null) || !hasMoreTokens) {
-				
-				if(hasMoreTokens) {
+			log.info("Indexando documento ["+document.getFileName()+"]");
+			
+			//Elimino tags y caracteres que no correspondan
+			inputFile = new File(Constants.FOLDER_DOCUMENTS + File.separator + document.getFileName());
+			
+			String documentText = DigesterUtils.getFormattedHtmlFile(inputFile);
+			
+			//Inicializo el pipeline para encolar terminos 
+			StopwordsPipelineDto stopwordsPipeline = new StopwordsPipelineDto();
+			
+			//Me inicializo un tokenizer para recorrer los terminos del documento
+			StringTokenizer strTok = new StringTokenizer(documentText, " \t\n\r\f");
+	
+			//Hago un preload de terminos en el pipeline
+			while(strTok.hasMoreTokens() && stopwordsPipeline.getSize()<(stopwordsPipeline.getMaxSize()-1)) {
+				String token = (strTok.nextToken()).trim();
+				if (!"".equals(token) && token!=null) {
 					stopwordsPipeline.addWord(token);
 				}
-
-				if(stopwordsPipeline.isFull() || !hasMoreTokens) {
+			}
+			
+			while(strTok.hasMoreTokens() || !stopwordsPipeline.isEmpty()) {
+				
+				boolean hasMoreTokens = false;
+				String token = null;
+				if(strTok.hasMoreTokens()) {
+					 token = (strTok.nextToken()).trim();
+					 hasMoreTokens = true;
+				}
+	
+				//Verifico que exista el termino
+				if ((!"".equals(token) && token!=null) || !hasMoreTokens) {
 					
-					int bestWordCompare = 0;
-					int wordCompare = 0;
-					//Recorro los stopwords y verifico si el/los termino/s son un stopword
-					for(WordDto stopword : stopwordsProcessor.getStopwords()) {
-
-						wordCompare = compareWords(stopwordsPipeline.getWords(),stopword.getWord());
-						
-						if(wordCompare>bestWordCompare) {
-							bestWordCompare = wordCompare;
-						}
-						
-						if(bestWordCompare>0 && wordCompare<bestWordCompare) {
-							break;
-						}
-						
+					if(hasMoreTokens) {
+						stopwordsPipeline.addWord(token);
 					}
-					
-					if(bestWordCompare==0) {
-						//El primer termino del pipeline no es stopword, por lo tanto se indexa
-						String indexWord = stemmingProcessor.stem(stopwordsPipeline.getFirstWord());
-						docInsert.insertWord(indexWord);
-
-						log.info(logtag+" "+document.getFileName()+"] --> [INDEXED: "+indexWord+"] [RAW WORD: "+stopwordsPipeline.getFirstWord()+"]");
-//						log.info("["+(i+1)+"/"+newDocuments.length+" "+document.getFileName()+"] --> [INDEXED: "+indexWord+"] [RAW WORD: "+stopwordsPipeline.getFirstWord()+"]");
-						totalIndexed++;
-						stopwordsPipeline.removeWords(1);
-					} else {
-						//Elimino stopword encontrado en pipeline
-						String eliminar = "";
-						for(int j=0;j<bestWordCompare;j++) {
-							eliminar+=stopwordsPipeline.getWord(j)+" ";
+	
+					if(stopwordsPipeline.isFull() || !hasMoreTokens) {
+						
+						int bestWordCompare = 0;
+						int wordCompare = 0;
+						//Recorro los stopwords y verifico si el/los termino/s son un stopword
+						for(WordDto stopword : stopwordsProcessor.getStopwords()) {
+	
+							wordCompare = compareWords(stopwordsPipeline.getWords(),stopword.getWord());
+							
+							if(wordCompare>bestWordCompare) {
+								bestWordCompare = wordCompare;
+							}
+							
+							if(bestWordCompare>0 && wordCompare<bestWordCompare) {
+								break;
+							}
+							
 						}
-						log.info(logtag+" "+document.getFileName()+"] [STOPWORD: "+eliminar.trim()+"]");
-						stopwordsPipeline.removeWords(bestWordCompare);
+						
+						if(bestWordCompare==0) {
+							//El primer termino del pipeline no es stopword, por lo tanto se indexa
+							String indexWord = stemmingProcessor.stem(stopwordsPipeline.getFirstWord());
+							docInsert.insertWord(indexWord);
+	
+							log.info(logtag+" "+document.getFileName()+"] --> [INDEXED: "+indexWord+"] [RAW WORD: "+stopwordsPipeline.getFirstWord()+"]");
+	//						log.info("["+(i+1)+"/"+newDocuments.length+" "+document.getFileName()+"] --> [INDEXED: "+indexWord+"] [RAW WORD: "+stopwordsPipeline.getFirstWord()+"]");
+							totalIndexed++;
+							stopwordsPipeline.removeWords(1);
+						} else {
+							//Elimino stopword encontrado en pipeline
+							String eliminar = "";
+							for(int j=0;j<bestWordCompare;j++) {
+								eliminar+=stopwordsPipeline.getWord(j)+" ";
+							}
+							log.info(logtag+" "+document.getFileName()+"] [STOPWORD: "+eliminar.trim()+"]");
+							stopwordsPipeline.removeWords(bestWordCompare);
+						}
+	
 					}
-
 				}
 			}
+			docInsert.flush();
+			return totalIndexed;
+		
+		} catch(ParserException pe) {
+			throw new BusinessException("Error obteniendo HTML formateado de [FILE:"+inputFile.getAbsolutePath()+"]",pe);
+		} catch(PipelineOverflowException poe) {
+			throw new BusinessException("Error por overflow en el pipeline de terminos a indexar",poe);
 		}
-		docInsert.flush();
-		return totalIndexed;
+
 	
 	}
 
@@ -153,49 +156,48 @@ public class DocumentsIndexer {
 		//Preparo los nuevos documentos a indexar
 		File[] newDocuments = DocumentsDictionaryImp.prepareNewDocuments(path);
 
+		//Preparo los documentos que ya fueron indexados
+		IndexedDocumentChecker indexedDocuments;
 		try {
-		
-			//Preparo los documentos que ya fueron indexados
-			IndexedDocumentChecker indexedDocuments = queryEngine.getDocuments();
-
-			log.info("<<< Inicio de indexacion de documentos >>>\n");
-
-			//Recorro los nuevos documentos a indexar
-			for (int i=0;i<newDocuments.length;i++) {
-
-				
-				long c1 = System.currentTimeMillis();
-				
-				DocumentDto document = new DocumentDto(newDocuments[i].getName());
-				
-				log.info("---------------------------------------------------------------------");
-				log.info("Intentando indexar documento ["+document.getFileName()+"]");
-
-				//Verifico que el documento a indexar, no este dentro de los ya indexados
-				if (!indexedDocuments.documentIsIndexed(document.getFileName())) {
-					
-					int totalIndexed = this.indexDocument(document,"["+(i+1)+"/"+newDocuments.length);
-					//Muevo el documento indexado a la carpeta de documentos indexados
-					DocumentsDictionaryImp.moveFileToIndexedFolder(newDocuments[i]);
-
-					long c2 = System.currentTimeMillis();
-					
-					//Manejo de estadisticas por documento, seteo total de terminos y tiempos de indexacion
-					statistics.addDocumentStatistics(document, totalIndexed, c2-c1);
-					
-					log.info("Fin de indexacion del documento ["+document.getFileName()+"]");
-				} else {
-					log.info("El documento ["+document.getFileName()+"] ya fue indexado anteriormente");
-				}
-			}
-			
-			log.info("\n<<< Fin de indexacion de documentos >>>");
-			
-			System.out.println(statistics.toString());
-			
+			indexedDocuments = queryEngine.getDocuments();
 		} catch (BusinessException e) {
 			throw new Exception("Error preparando nuevos documentos pendientes a indexar",e);
 		}
+
+		log.info("<<< Inicio de indexacion de documentos >>>\n");
+
+		//Recorro los nuevos documentos a indexar
+		for (int i=0;i<newDocuments.length;i++) {
+
+			
+			long c1 = System.currentTimeMillis();
+			
+			DocumentDto document = new DocumentDto(newDocuments[i].getName());
+			
+			log.info("---------------------------------------------------------------------");
+			log.info("Intentando indexar documento ["+document.getFileName()+"]");
+
+			//Verifico que el documento a indexar, no este dentro de los ya indexados
+			if (!indexedDocuments.documentIsIndexed(document.getFileName())) {
+				
+				int totalIndexed = this.indexDocument(document,"["+(i+1)+"/"+newDocuments.length);
+				//Muevo el documento indexado a la carpeta de documentos indexados
+				DocumentsDictionaryImp.moveFileToIndexedFolder(newDocuments[i]);
+
+				long c2 = System.currentTimeMillis();
+				
+				//Manejo de estadisticas por documento, seteo total de terminos y tiempos de indexacion
+				statistics.addDocumentStatistics(document, totalIndexed, c2-c1);
+				
+				log.info("Fin de indexacion del documento ["+document.getFileName()+"]");
+			} else {
+				log.info("El documento ["+document.getFileName()+"] ya fue indexado anteriormente");
+			}
+		}
+		
+		log.info("\n<<< Fin de indexacion de documentos >>>");
+		
+		System.out.println(statistics.toString());
 		
 	}
 
