7ca8bfbc1586e7eeb2a6cb758b684ca61f61c68d
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-11-18 23:53:30 +0000

    eliminada sobreescritura innecesaria de un metodo

--- tpdatos/src/app/query/parser/QueryParser.java
+++ tpdatos/src/app/query/parser/QueryParser.java
@@ -1,16 +1,9 @@
 package app.query.parser;
 
-import api.query.tree.Query;
-import app.query.parser.exception.ParserException;
-
 public class QueryParser extends CompositeParser {
 
 	public QueryParser() {
 		this.add(new QueryWordParser() );
-	}
-	
-	public Query parse(String str) throws ParserException {
-		return null;
 	}
 
 }
