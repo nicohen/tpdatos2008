46a63a8ef1701a8df1361f886d5d1da320de375a
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-11-19 02:08:14 +0000

    impresion de un arbol en parser test

--- trunk/tpdatos/src/test/query/ParserTest.java
+++ trunk/tpdatos/src/test/query/ParserTest.java
@@ -1,11 +1,26 @@
 package test.query;
 
+import app.query.QueryWord;
 import app.query.parser.QueryParser;
+import app.query.tree.QueryAnd;
 import junit.framework.TestCase;
 
 public class ParserTest extends TestCase {
-	void test() {
-		QueryParser parser = new QueryParser();
+	public void test() {
+		
+		QueryAnd queryAnd = new QueryAnd();
+		
+		queryAnd.add(new QueryWord("hola"));
+		queryAnd.add(new QueryWord("mundo"));
+		
+		// salida esperada:
+		// and
+		//		Word: hola
+		//		Word: mundo
+		
+		queryAnd.dump(0);
+		
+		//QueryParser parser = new QueryParser();
 		
 		
 	}
