14a150c059a82dffc0a470ded21231a322107a86
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-11-19 01:44:33 +0000

    logica de parseo implementada en CompositeQueryParser

--- trunk/tpdatos/src/app/query/parser/CompositeQueryParser.java
+++ trunk/tpdatos/src/app/query/parser/CompositeQueryParser.java
@@ -1,24 +1,67 @@
 package app.query.parser;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import api.query.parser.Parser;
 import api.query.tree.Query;
+import app.query.parser.exception.CantDigestException;
 import app.query.parser.exception.ParserException;
 import app.query.tree.CompositeQuery;
 
 abstract public class CompositeQueryParser extends RecursiveParser {
 	
-	private String token;
+	private String separator;
 
-	public CompositeQueryParser(Parser recur, String token) {
+	public CompositeQueryParser(Parser recur, String separator) {
 		super(recur);
 		
-		this.token = token; 
+		this.separator = separator; 
 	}
 
 	public Query parse(String str) throws ParserException {
 		// TODO dividir la cadena por tokens, y parsear
 		// recursivamente
-		return null;
+
+		List<String> tokens = new LinkedList<String>();
+		
+		for (int i=1; i<str.length()-separator.length()-1; i++) {
+			
+			// TODO: ignorar el separator si esta entre parentesis
+			
+			// si el substring tiene el token
+			if (str.substring(i,i+separator.length() ).equals(separator) ) {
+				// y si ademas , esta rodeado por espacios
+				if (str.charAt(i-1)==' ' && str.charAt(i+separator.length()+1)==' ') {
+					// agregar los dos tokens y salir
+					
+					String newtoken;
+					
+					newtoken = str.substring(0,i-1);
+					tokens.add(newtoken);
+					
+					newtoken = str.substring(i+separator.length()+1,str.length());
+					tokens.add(newtoken);
+					
+					break;
+				}
+			}
+		}
+		
+		if (tokens.size() < 2  ) {
+			// este parser no puede parsear algo de menos de dos tokens
+			throw new CantDigestException();
+		}
+		
+		CompositeQuery query = createComposite();
+		
+		for (String token : tokens ) {
+			// agregar a la query compuesta la query resultante
+			// de parsear cada token
+			query.add( this.internalParse(token) );
+		}
+		
+		return query;
 	}
 	
 	abstract public CompositeQuery createComposite() ;
