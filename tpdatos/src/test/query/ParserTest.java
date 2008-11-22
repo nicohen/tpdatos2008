6b86d35fcd67689a7f8d7c75426480629eac01f9
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-11-22 20:02:36 +0000

    agregado QueryNot en la construccion de queryparser en parsertest

--- tpdatos/src/test/query/ParserTest.java
+++ tpdatos/src/test/query/ParserTest.java
@@ -4,13 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+
+import junit.framework.TestCase;
+import api.query.parser.Parser;
 import api.query.tree.Query;
 import app.query.QueryWord;
+import app.query.parser.QueryNotParser;
 import app.query.parser.QueryParser;
 import app.query.parser.QueryWordParser;
 import app.query.parser.exception.ParserException;
 import app.query.tree.QueryAnd;
-import junit.framework.TestCase;
+import app.query.tree.QueryNot;
 
 public class ParserTest extends TestCase {
 	private QueryParser parser ;
@@ -35,10 +39,38 @@ public class ParserTest extends TestCase {
 		}
 		
 	}
+
 	
+	class TestQueryNot extends QueryNot {
+
+		public TestQueryNot(Query subQuery) {
+			super(subQuery);
+			// TODO Auto-generated constructor stub
+		}
+		@Override
+		public int getDocumentsCount() {
+			return 0 ;
+		}
+		
+	}
+	
+	class TestQueryNotParser extends QueryNotParser {
+
+		public TestQueryNotParser(Parser recur) {
+			super(recur);
+		}
+
+		@Override
+		protected Query createQueryNot(Query consulta) {
+			return new TestQueryNot(consulta);
+		}
+		
+	}
+		
 	public ParserTest() {
 		parser = new QueryParser();
 		parser.addCustomParser(new TestQueryWordParser() );
+		parser.addCustomParser(new TestQueryNotParser(parser) );
 	}
 	
 	public void test() throws ParserException {
