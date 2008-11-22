237324600a3b86753ed6f9f9afb5f52a71082c54
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-11-22 16:09:33 +0000

    corregido error, de querys del estilo "(hola) and (chau)" se malinterpretaban como la busqueda de un termino "hola) and (chau" cuando lo interpretaba el parser de parentesis

--- tpdatos/src/app/query/parser/QueryWordParser.java
+++ tpdatos/src/app/query/parser/QueryWordParser.java
@@ -10,6 +10,10 @@ abstract public class QueryWordParser implements Parser {
 
 	public Query parse(String str) throws ParserException {
 		
+		// los caracteres especiales de ) y ( no son tolerados
+		// en un string
+		if (str.contains(")") ) throw new CantDigestException();
+		if (str.contains("(") ) throw new CantDigestException();
 		if (str.equals("") ) throw new CantDigestException();
 		// todo es un word, para este parser
 		return create(str);
