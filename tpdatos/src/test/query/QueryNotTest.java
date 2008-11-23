458d18686af4c1de762eee2bfac47e6488522a97
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-11-23 00:19:02 +0000

    funciona version mas liviana de QueryNot

--- tpdatos/src/test/query/QueryNotTest.java
+++ tpdatos/src/test/query/QueryNotTest.java
@@ -38,5 +38,11 @@ public class QueryNotTest extends QueryTestCase {
 	public void test() {
 		int[][] a = { {1,2 }, {0,3,4,5,6,7,8,9,10,11,12,13,14,15 } };
 		_test_not(a);
+
+		int[][] b = { {2,3,9,10 }, {0,1,4,5,6,7,8,11,12,13,14,15 } };
+		_test_not(b);
+
+		int[][] c = { {4,5,7 }, {0,1,2,3,6,8,9,10,11,12,13,14,15 } };
+		_test_not(c);
 	}
 }
