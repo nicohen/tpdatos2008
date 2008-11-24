4cd018c3ace83ffb9f10dcfd390849bd3e485e1d
pablonazareno <pablonazareno@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-11-24 18:17:12 +0000

    Primera apariencia grafica del indexador

--- trunk/tpdatos/src/web/indexer/IndexerView.java
+++ trunk/tpdatos/src/web/indexer/IndexerView.java
@@ -1,7 +1,13 @@
 package web.indexer;
+import java.io.File;
+import java.util.Map.Entry;
+
+import processor.utils.HtmlUtils;
 import utils.Constants;
 import utils.statistics.Statistics;
 import web.View;
+import dto.DocumentDto;
+import dto.DocumentStatisticsDto;
 
 
 public class IndexerView extends View {
@@ -13,10 +19,35 @@ public class IndexerView extends View {
 	}
 
 	@Override
-	protected String doHtmlBody(String basePath) {
+	protected String doHtmlBody(String basePath) {		
 		try {
+			File documentsResultRowFile = new File(basePath+"/html/index_body.html");
+			String indexContentHtml = HtmlUtils.readHtmlFile(documentsResultRowFile.getPath());
+			documentsResultRowFile = new File(basePath+"/html/index_doc.html");
+			String indexDocumentHtml = HtmlUtils.readHtmlFile(documentsResultRowFile.getPath());
+			documentsResultRowFile = new File(basePath+"/html/index_stats.html");
+			String indexStatsHtml = HtmlUtils.readHtmlFile(documentsResultRowFile.getPath());
+			long totalWords=0;
+			long totalTime=0;
+			StringBuffer sb= new StringBuffer();
 			Statistics stats= model.indexPath(Constants.FOLDER_DOCUMENTS);
-			return stats.toString();
+			for(Entry<DocumentDto,DocumentStatisticsDto> documentEntry : stats.getDocumentsStatistics().entrySet()) {
+				DocumentStatisticsDto statistic = documentEntry.getValue();
+				String actualDoc= indexDocumentHtml.replace("##DOC_NAME##", documentEntry.getKey().getFileName());
+				actualDoc= actualDoc.replace("##DOC_WORDS##", Integer.toString(statistic.getWordsQuantity()));
+				actualDoc= actualDoc.replace("##DOC_TIME##", Long.toString(statistic.getIndexTime()));
+				totalTime+=statistic.getIndexTime();
+				totalWords+=statistic.getWordsQuantity();
+				sb.append(actualDoc);
+			}
+			indexStatsHtml= indexStatsHtml.replaceAll("##RBYTES##", Long.toString(stats.getReadedBytes()));
+			indexStatsHtml= indexStatsHtml.replaceAll("##WBYTES##", Long.toString(stats.getWrittenBytes()));
+			indexStatsHtml= indexStatsHtml.replaceAll("##WORDS##", Long.toString(totalWords));
+			indexStatsHtml= indexStatsHtml.replaceAll("##TIME##", Long.toString(totalTime));
+			
+			indexContentHtml= indexContentHtml.replace("##DOCUMENTS_INFO##", sb.toString());
+			indexContentHtml= indexContentHtml.replace("##INDEX_INFO##", indexStatsHtml);
+			return indexContentHtml;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
