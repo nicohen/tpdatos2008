c34255f53746a3f8b1efd883d8eee8dc55c41be3
berta1108 <berta1108@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-11-26 20:46:12 +0000

    Reemplazo de muchos hashsets por 1 solo. Se recorre con cada iterator de resultados a mergear y se agregan a 1 hashset general con los resultados.

--- tpdatos/src/app/query/tree/QueryAnd.java
+++ tpdatos/src/app/query/tree/QueryAnd.java
@@ -13,60 +13,27 @@ public class QueryAnd extends CompositeQuery {
 	String operatorName() {
 		return "and";
 	}
-
+//Optimizar QueryAnd
+//Stemming cuando indexa
 	public Iterator<Integer> iterator() {
 
 		// generar un hashset de cada iterator, excepto para el primero
 		
 		List<Query> querys = this.getQuerys();
+		HashSet<Integer> resultadoMerge=new HashSet<Integer>();
 		
-		if (querys.size() == 0) return new ArrayList<Integer>().iterator();
+		if (querys.size() == 0) return new HashSet<Integer>().iterator();
 		if (querys.size() == 1) return querys.get(0).iterator();
 		
-		List<HashSet<Integer> > hashsets = 
-			new ArrayList<HashSet<Integer> >();
 		
-		boolean esprimera = true;
-		Iterator<Integer> it_primera = null;
-		
-		for (Query q : querys) {
-			if (esprimera) {
-				it_primera = q.iterator();
-				esprimera = false;
-			} else {
-				HashSet<Integer> new_hashset = new HashSet<Integer>();
-				
-				Iterator<Integer> it = q.iterator();
-				while (it.hasNext() ) {
-					new_hashset.add(it.next() );
-				}
-				
-				hashsets.add(new_hashset);
+		for (Query q : querys){
+			Iterator<Integer> it = q.iterator();
+			while(it.hasNext()){
+				resultadoMerge.add(it.next());
 			}
 		}
 		
-		List<Integer> lista = new ArrayList<Integer> ();
-		
-		while (it_primera.hasNext() ) {
-			boolean todos = true;
-			
-			Integer id = it_primera.next();
-			
-			for ( HashSet<Integer> hashset : hashsets) {
-				if (! hashset.contains(id )) {
-					todos = false;
-					break;
-				}
-			}
-			
-
-			if (todos) {
-				lista.add(id);
-			}
-			
-		}
-		
-		return lista.iterator();
+		return resultadoMerge.iterator();
 	}
 
 
