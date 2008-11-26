348115fc25ab94077c2fec6ea95344c0f2ec40c7
nicohen <nicohen@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-11-26 21:51:10 +0000

    Agregados casos a QueryAnd

--- tpdatos/src/test/query/QueryAndTest.java
+++ tpdatos/src/test/query/QueryAndTest.java
@@ -8,6 +8,15 @@ public class QueryAndTest extends CompositeQueryTest {
 		int[][] a = { {1,2,3}, {2,3,4}, {2,3} };
 		_test(a);
 
+		int[][] b = { {1,2,3,4}, {2,3,4}, {2,3,4} };
+		_test(b);
+
+		int[][] c = { {1,6,7}, {2,3,4,5,6,7,8}, {6,7} };
+		_test(c);
+		
+		int[][] d = { {1,20,30}, {2,4}, {} };
+		_test(d);
+		
 	}
 
 	@Override
