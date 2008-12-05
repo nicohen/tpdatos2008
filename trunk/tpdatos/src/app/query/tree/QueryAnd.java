048dd066329f9a9ba6e22aff4807e18dcc6688cb
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-12-05 01:19:06 +0000

    eliminados hashsets de queryand (funciona haciendo una interseccion secuencial de los datos)

--- trunk/tpdatos/src/app/query/tree/QueryAnd.java
+++ trunk/tpdatos/src/app/query/tree/QueryAnd.java
@@ -13,6 +13,92 @@ public class QueryAnd extends CompositeQuery {
 	String operatorName() {
 		return "and";
 	}
+	
+	private class MergeIterator implements Iterator<Integer> {
+		
+		private Iterator<Integer> it1, it2;
+		
+		Integer lastIt1, lastIt2;
+		
+		boolean hasNext;
+		Integer next;
+		
+		public MergeIterator(Iterator<Integer> it1, Iterator<Integer > it2) {
+			this.it1 = it1;
+			this.it2 = it2;
+			this.hasNext = false;
+			this.next = 0;
+			this.lastIt1 = 0;
+			this.lastIt2 = 0;
+			
+			searchNext();
+			
+		}
+		public boolean hasNext() {
+			return this.hasNext;
+		}
+		public Integer next() {
+			Integer aux = next;
+			searchNext();
+			return aux;
+		}
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		public void searchNext() {
+			
+			this.hasNext = false;
+			while (true) {
+				if (lastIt1 < lastIt2) {
+					// obtener uno nuevo de lastIt1
+					if (it1.hasNext() ) {
+						this.lastIt1 = it1.next();
+					} else {
+						this.hasNext = false;
+						break;
+					}
+					
+				} else if (lastIt1 > lastIt2 ){
+					if (it2.hasNext() ) {
+						this.lastIt2 = it2.next();
+					} else {
+						this.hasNext = false;
+						break;
+					}
+					
+				} else {
+					// ambos son 0
+					if (it1.hasNext() ) {
+						this.lastIt1 = it1.next();
+					} else {
+						this.hasNext = false;
+						break;
+					}
+					if (it2.hasNext() ) {
+						this.lastIt2 = it2.next();
+					} else {
+						this.hasNext = false;
+						break;
+					}
+					
+				}
+				
+				if (this.lastIt1.equals( this.lastIt2 ) ) {
+					this.hasNext = true;
+					this.next = this.lastIt1;
+					break;
+				}
+			}
+		}
+	}
+	
+	private Iterator<Integer> mergeIterator( Iterator<Integer> it1, Iterator<Integer> it2) {
+		
+		return new MergeIterator(it1,it2);
+	
+	}
 
 	public Iterator<Integer> iterator() {
 
@@ -22,52 +108,17 @@ public class QueryAnd extends CompositeQuery {
 		
 		if (querys.size() == 0) return new ArrayList<Integer>().iterator();
 		if (querys.size() == 1) return querys.get(0).iterator();
-		
-		List<HashSet<Integer> > hashsets = 
-			new ArrayList<HashSet<Integer> >();
-		
-		boolean esprimera = true;
-		Iterator<Integer> it_primera = null;
-		
-		for (Query q : querys) {
-			if (esprimera) {
-				it_primera = q.iterator();
-				esprimera = false;
-			} else {
-				HashSet<Integer> new_hashset = new HashSet<Integer>();
-				
-				Iterator<Integer> it = q.iterator();
-				while (it.hasNext() ) {
-					Integer id = it.next() ;
-					new_hashset.add(id);
-				}
-				
-				hashsets.add(new_hashset);
-			}
-		}
-		
-		List<Integer> lista = new ArrayList<Integer> ();
-		
-		while (it_primera.hasNext() ) {
-			boolean todos = true;
-			
-			Integer id = it_primera.next();
-			
-			for ( HashSet<Integer> hashset : hashsets) {
-				if (! hashset.contains(id )) {
-					todos = false;
-					break;
-				}
-			}
-			
 
-			if (todos) {
-				lista.add(id);
+		// componer una red de mergeIterator
+		Iterator<Integer> lastIterator = null;
+		for (Query q : querys ) {
+			if (lastIterator == null) {
+				lastIterator = q.iterator();
+			} else {
+				lastIterator = mergeIterator(lastIterator,q.iterator());
 			}
-			
 		}
-		
-		return lista.iterator();
+		return lastIterator;
 	}
 
 
