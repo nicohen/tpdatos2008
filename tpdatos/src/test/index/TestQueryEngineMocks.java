91d4fd4f06c471c6421f3051d9f7f0ddbb1769c6
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-12-06 15:59:42 +0000

    correcciones a TestIndex (dead-code revivido) y atacados warnings en TestQueryEngineMocks

--- tpdatos/src/test/index/TestQueryEngineMocks.java
+++ tpdatos/src/test/index/TestQueryEngineMocks.java
@@ -1,11 +1,8 @@
 package test.index;
 
-import api.Index;
 import api.MockQueryEngine;
 
 public class TestQueryEngineMocks extends TestQueryEngine {
-	
-	private Index index;
 	
 	public TestQueryEngineMocks() {
 		super(
