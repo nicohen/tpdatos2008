7679bbc42e1327593b7396ccd68bd1686da99f2b
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-12-06 16:00:29 +0000

    corregido warnings relacionados con veriables no usadas o imports no usados a lo largo del codigo

--- tpdatos/src/app/bo/bplus/BPlusTreeBoImp.java
+++ tpdatos/src/app/bo/bplus/BPlusTreeBoImp.java
@@ -5,12 +5,7 @@ import exceptions.DataAccessException;
 
 public class BPlusTreeBoImp extends AbstractBPlusTreeBo {
 
-	private String filename;
-	private int nodeSize;
-	
 	public BPlusTreeBoImp( String filename, int nodeSize ) throws DataAccessException {
-		this.filename= filename;
-		this.nodeSize = nodeSize;
 		setDao(new BPlusNodeDaoImp(filename,nodeSize));
 	}
 	
