204ace0b574d7ee37ecf4a3b1d5aa3551ad97373
darios3 <darios3@ce6c4afa-f348-0410-9719-5941d86a54b7>
2008-12-07 18:15:29 +0000

    quitado metodo executeQuery ( con el iterator ya es suficiente )

--- tpdatos/src/api/IQueryEngine.java
+++ tpdatos/src/api/IQueryEngine.java
@@ -1,7 +1,6 @@
 package api;
 
 import java.util.Iterator;
-import java.util.List;
 
 import processor.IndexedDocumentChecker;
 import dto.DocumentDto;
@@ -13,12 +12,6 @@ public interface IQueryEngine {
 			throws BusinessException;
 	
 	public abstract Iterator<DocumentDto> iteratorQuery(String consulta) throws BusinessException;
-
-	public abstract List<DocumentDto> executeQuery(String consulta, int offset, int limit)
-			throws BusinessException;
-
-	public abstract List<DocumentDto> executeQuery(String consulta)
-			throws BusinessException;
 	
 	public abstract DocumentInsert prepareDocumentInsert(String documento)
 			throws BusinessException;
